9696bbf8c0e45e24f16c1092cee7e6f16b106a69
AngryCracker <ads6may@gmail.com>
2017-06-22 01:06:48 +0530

    multiple scatter plots

--- Data Visualization/scatter_plot_3d.py
+++ Data Visualization/scatter_plot_3d.py
@@ -5,7 +5,9 @@ fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 X, Y, Z = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [5, 6, 2, 3, 13, 4, 1, 2, 4, 8], [2, 3, 3, 3, 5, 7, 9, 11, 9, 10]
+Xs, Ys, Zs = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10], [-5, -6, -2, -3, -13, -4, -1, -2, -4, -8], [-2, -3, -3, -3, -5, -7, -9, -11, -9, -10]
 ax.scatter(X, Y, Z, c='r', marker='o')
+ax.scatter(Xs, Ys, Zs, c='b', marker='^')
 
 ax.set_xlabel('x axis')
 ax.set_ylabel('y axis')
