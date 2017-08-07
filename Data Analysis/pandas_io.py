2748f0461ec8abe43f2ec82e17d466e411274fca
AngryCracker <ads6may@gmail.com>
2017-08-07 15:56:10 +0530

    minor

--- Data Analysis/pandas_io.py
+++ Data Analysis/pandas_io.py
@@ -3,7 +3,7 @@ import pandas as pd
 df = pd.read_csv('C:\\Users\\Aman Deep Singh\\Documents\\Python\\Data Science\\Data Analysis\\housing77006.csv', index_col=0)
 print(df.head())
 
-df.set_index('Date', inplace=True)
-df.to_csv('C:\\Users\\Aman Deep Singh\\Documents\\Python\\Data Science\\Data Analysis\\housing77006_v2.csv')
+# df.set_index('Date', inplace=True)
+# df.to_csv('C:\\Users\\Aman Deep Singh\\Documents\\Python\\Data Science\\Data Analysis\\housing77006_v2.csv')
 # Even though we defined the 'Date' column to be the index, when we import it again, nothing changes, so we 
 # can define the index column inside the read_csv function just as we are importing the dataframe
