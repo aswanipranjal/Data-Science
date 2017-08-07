32578b32f4608a9931b7ff60c4a9d27f8b096325
AngryCracker <ads6may@gmail.com>
2017-08-07 16:53:40 +0530

    minor

--- Data Analysis/pandas_io.py
+++ Data Analysis/pandas_io.py
@@ -6,4 +6,10 @@ print(df.head())
 # df.set_index('Date', inplace=True)
 # df.to_csv('C:\\Users\\Aman Deep Singh\\Documents\\Python\\Data Science\\Data Analysis\\housing77006_v2.csv')
 # Even though we defined the 'Date' column to be the index, when we import it again, nothing changes, so we 
-# can define the index column inside the read_csv function just as we are importing the dataframe
+# can define the index column inside the read_csv function just as we are importing the dataframe.
+
+# To rename every single column
+# Now, df.columns does not include the 'Date' column. It is now an index. Indexes and columns are different
+# HPI: House Price Index
+df.columns = ['Austin_HPI']
+print(df.head())
