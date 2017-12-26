6217aad4bb76a61e3bdef7b448b78f3cf45144c0
AngryCracker <ads6may@gmail.com>
2017-12-26 18:30:39 +0530

    generator class created

--- Machine Learning/Intro to DL/earthquake_data_analysis.py
+++ Machine Learning/Intro to DL/earthquake_data_analysis.py
@@ -115,3 +115,35 @@ class Math:
 
 		return new_x
 
+class Generator:
+	@staticmethod
+	def gen_random_batch(batch_size, X, Y):
+		'''
+		Generator for random batch
+		:param batch_size: size of the returned batches
+		:param X: X array
+		:param Y: Y array
+		:return: random batches of the given size
+		'''
+		while True:
+			index = np.arange(X.shape[0])
+			np.random.shuffle(index)
+
+			s_X, s_Y = X[index], Y[index]
+			for i in range(X.shape[0] // batch_size):
+				yield (X[i * batch_size:(i + 1) * batch_size], Y[i * batch_size:(i + 1) * batch_size])
+
+	@staticmethod
+	def get_batch(batch_size, X, Y):
+		'''
+		Generator to split given arrays in smaller batches
+		:param batch_size: size of the returned batches
+		:param X: X array
+		:param Y: Y array
+		:return: random batches of the given size
+		'''
+		if X.shape[0] % batch_size != 0:
+			print("Warning: The full set will not be executed because of a poor choice of batch_size")
+
+		for i in range(X.shape[0] // batch_size):
+			yield X[i * batch_size:(i + 1) * batch_size], Y[i * batch_size:(i + 1) * batch_size]
