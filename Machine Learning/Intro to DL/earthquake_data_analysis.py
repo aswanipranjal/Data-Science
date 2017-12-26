e3b2ce808f4a84ea86af2bc19c0aecadde3dfdac
AngryCracker <ads6may@gmail.com>
2017-12-26 18:31:36 +0530

    minor

--- Machine Learning/Intro to DL/earthquake_data_analysis.py
+++ Machine Learning/Intro to DL/earthquake_data_analysis.py
@@ -147,3 +147,6 @@ class Generator:
 
 		for i in range(X.shape[0] // batch_size):
 			yield X[i * batch_size:(i + 1) * batch_size], Y[i * batch_size:(i + 1) * batch_size]
+
+if __name__ == "__main__":
+	
