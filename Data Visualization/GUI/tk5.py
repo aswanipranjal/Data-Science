e07f1d617207cf95533ba899ac29096e0e50eb64
AngryCracker <ads6may@gmail.com>
2018-01-12 00:45:53 +0530

    minor

--- Data Visualization/GUI/tk5.py
+++ Data Visualization/GUI/tk5.py
@@ -5,7 +5,14 @@ root = Tk()
 def print_name():
 	print('Hello bitch!')
 
+def print_name_event(event):
+	print('Hello fucking bitch!')	
+
 button_1 = Button(root, text='Print my name', command=print_name)
 button_1.pack()
+
+button_2 = Button(root, text='Print my name, again')
+button_2.bind("<Button-1>", print_name_event)
+button_2.pack()
 
 root.mainloop()
