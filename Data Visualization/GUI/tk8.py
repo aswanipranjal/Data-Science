f52e65a26de5dd80e8f77f8d08dedaddbb77a112
AngryCracker <ads6may@gmail.com>
2018-01-12 02:34:40 +0530

    minor

---
+++ Data Visualization/GUI/tk8.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+def do_nothing():
+	print('Ok ok I won\'t')
+
+root = Tk()
+menu = Menu(root)
+root.config(menu=menu)
+sub_menu = Menu(menu)
+menu.add_cascade(label='File', menu=sub_menu)
+sub_menu.add_command(label='New Project', command=do_nothing)
+sub_menu.add_command(label='New', command=do_nothing)
+sub_menu.add_separator()
+sub_menu.add_command(label='Exit', command=do_nothing)
+root.mainloop()
