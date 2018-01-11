b751201ac0848a9d7021c7166fcaa1c27d1bc872
AngryCracker <ads6may@gmail.com>
2018-01-12 02:35:52 +0530

    minor

--- Data Visualization/GUI/tk8.py
+++ Data Visualization/GUI/tk8.py
@@ -12,4 +12,8 @@ sub_menu.add_command(label='New Project', command=do_nothing)
 sub_menu.add_command(label='New', command=do_nothing)
 sub_menu.add_separator()
 sub_menu.add_command(label='Exit', command=do_nothing)
+
+edit_menu = Menu(menu)
+menu.add_cascade(label='Edit', menu=edit_menu)
+edit_menu.add_command(label='Redo', command=do_nothing)
 root.mainloop()
