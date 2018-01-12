26c8343093a8f3e5529ab370553cceaa7d118baa
AngryCracker <ads6may@gmail.com>
2018-01-12 16:25:54 +0530

    extremely pointless

--- Data Visualization/GUI/tk9.py
+++ Data Visualization/GUI/tk9.py
@@ -4,5 +4,10 @@ import tkinter.messagebox
 root = Tk()
 
 tkinter.messagebox.showinfo('Window Title', 'I can lie if I want to')
+answer = tkinter.messagebox.askquestion('Question 1', 'But does it djent?')
+if answer == 'yes':
+	print('8=====D')
+else:
+	print('8=D')
 
 root.mainloop()
