46adaea936a9b76533b1d593e830b3cfed5497e2
AngryCracker <ads6may@gmail.com>
2018-01-12 16:37:19 +0530

    minor

--- Data Visualization/GUI/tk10.py
+++ Data Visualization/GUI/tk10.py
@@ -8,5 +8,8 @@ black_line = canvas.create_line(0, 0, 100, 50)
 red_line = canvas.create_line(100, 50, 150, 75, fill='red')
 blue_line = canvas.create_line(150, 75, 175, 87.5, fill='blue')
 green_box = canvas.create_rectangle(175, 87.5, 200, 100, fill='green')
+# to delete one entity
 canvas.delete(red_line)
+# to delete everything
+canvas.delete(ALL)
 root.mainloop()
