74f5c5f16938c49f9bf712353bb1e954e8f8c802
AngryCracker <ads6may@gmail.com>
2018-01-12 16:11:05 +0530

    toolbar

--- Data Visualization/GUI/tk8.py
+++ Data Visualization/GUI/tk8.py
@@ -4,6 +4,8 @@ def do_nothing():
 	print('Ok ok I won\'t')
 
 root = Tk()
+
+# Menubar
 menu = Menu(root)
 root.config(menu=menu)
 sub_menu = Menu(menu)
@@ -12,6 +14,14 @@ sub_menu.add_command(label='New Project', command=do_nothing)
 sub_menu.add_command(label='New', command=do_nothing)
 sub_menu.add_separator()
 sub_menu.add_command(label='Exit', command=do_nothing)
+
+# Toolbar
+toolbar = Frame(root, bg='blue')
+insert_button = Button(toolbar, text='Insert Image', command=do_nothing)
+insert_button.pack(side=LEFT, padx=2, pady=2)
+print_button = Button(toolbar,text='Print',command=do_nothing)
+print_button.pack(side=LEFT, padx=2, pady=2)
+toolbar.pack(side=TOP, fill=X)
 
 edit_menu = Menu(menu)
 menu.add_cascade(label='Edit', menu=edit_menu)
