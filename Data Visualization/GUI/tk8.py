6790ff6af3f50aece458c6e107b17034cd1ae698
AngryCracker <ads6may@gmail.com>
2018-01-12 16:17:20 +0530

    status bar

--- Data Visualization/GUI/tk8.py
+++ Data Visualization/GUI/tk8.py
@@ -14,6 +14,9 @@ sub_menu.add_command(label='New Project', command=do_nothing)
 sub_menu.add_command(label='New', command=do_nothing)
 sub_menu.add_separator()
 sub_menu.add_command(label='Exit', command=do_nothing)
+edit_menu = Menu(menu)
+menu.add_cascade(label='Edit', menu=edit_menu)
+edit_menu.add_command(label='Redo', command=do_nothing)
 
 # Toolbar
 toolbar = Frame(root, bg='blue')
@@ -23,7 +26,8 @@ print_button = Button(toolbar,text='Print',command=do_nothing)
 print_button.pack(side=LEFT, padx=2, pady=2)
 toolbar.pack(side=TOP, fill=X)
 
-edit_menu = Menu(menu)
-menu.add_cascade(label='Edit', menu=edit_menu)
-edit_menu.add_command(label='Redo', command=do_nothing)
+# Status bar
+status = Label(root, text='Preparing to do nothing', bd=1, relief=SUNKEN, anchor=W)
+status.pack(side=BOTTOM, fill=X)
+
 root.mainloop()
