ece096e1a8a2021958d11a228cba045940c0cdc5
AngryCracker <ads6may@gmail.com>
2018-01-15 12:02:58 +0530

    minor

--- Data Visualization/Pygame/setup.py
+++ Data Visualization/Pygame/setup.py
@@ -1,10 +1,23 @@
 import cx_Freeze
+import sys
+import os
+import pygame
 
-executables = [cx_Freeze.Executable("pg2.py")]
+os.environ['TCL_LIBRARY'] = r'C:\Users\Aman Deep Singh\Anaconda3\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Users\Aman Deep Singh\Anaconda3\tcl\tk8.6'
+
+base = None
+
+if sys.platform == 'win32':
+	base = "Win32GUI"
+executables = [cx_Freeze.Executable("pg2.py", base=base)]
 
 cx_Freeze.setup(
 	name="Smart Rockets",
 	options={"build_exe": {"packages":["pygame"],
-						   "include_files":["racecar.png"]}},
-	executables=executables
+						   "include_files":["racecar.png",
+						   					"C:/Users/Aman Deep Singh/Anaconda3/DLLs/tcl86t.dll",
+						   					"C:/Users/Aman Deep Singh/Anaconda3/DLLs/tk86t.dll"]}},
+	executables=executables,
+	version='1.0.0'
 	)
