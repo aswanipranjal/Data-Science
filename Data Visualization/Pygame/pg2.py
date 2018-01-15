f9db0f49ecb23a5b68f5f95206dd1a2e333e74ca
AngryCracker <ads6may@gmail.com>
2018-01-15 13:25:02 +0530

    pause function doesn't work

--- Data Visualization/Pygame/pg2.py
+++ Data Visualization/Pygame/pg2.py
@@ -92,7 +92,7 @@ def button(msg, x, y, w, h, i_color, a_color, action=None):
 	m_text_rect.center = ((x + (w / 2)), (y + (h / 2)))
 	game_display.blit(m_text_surface, m_text_rect)
 
-def paused():
+def paused(pause):
 	large_text = pygame.font.Font('freesansbold.ttf', 115)
 	m_text_surface, m_text_rect = text_objects('Paused', large_text)
 	m_text_rect.center = ((display_width/2), (display_height/2))
@@ -159,7 +159,7 @@ def game_loop():
 					x_change = 5
 				if event.key == pygame.K_p:
 					pause = True
-					paused()
+					paused(pause)
 
 			if event.type == pygame.KEYUP:
 				if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
