dbd14a474a348ad0332053c7f616a1dc47bff717
AngryCracker <ads6may@gmail.com>
2018-01-15 10:49:52 +0530

    drawing random stuff

--- Data Visualization/Pygame/pg3.py
+++ Data Visualization/Pygame/pg3.py
@@ -15,6 +15,11 @@ game_display.fill(black)
 pix_ar = pygame.PixelArray(game_display)
 pix_ar[10][20] = green
 
+pygame.draw.line(game_display, blue, (100,  200), (300, 450), 5)
+pygame.draw.rect(game_display, red, (400, 400, 50, 25))
+pygame.draw.circle(game_display, white, (150, 150), 75)
+pygame.draw.polygon(game_display, green, ((25, 75), (76, 125), (250, 375), (400, 25), (60, 540)))
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
