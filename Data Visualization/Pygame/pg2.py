235b60860aae80346b47c1e92ca0453818c035db
AngryCracker <ads6may@gmail.com>
2018-01-15 09:51:59 +0530

    displaying a car image

--- Data Visualization/Pygame/pg2.py
+++ Data Visualization/Pygame/pg2.py
@@ -13,4 +13,25 @@ white = (255, 255, 255)
 
 clock = pygame.time.Clock()
 crashed = False
-car_img = pygame.image.load()
+car_img = pygame.image.load('C:/Users/Aman Deep Singh/Documents/Python/Data Science/Data Visualization/Pygame/racecar.png')
+
+def car(x, y):
+	game_display.blit(car_img, (x, y))
+
+# starting points for our car
+x = (display_width * 0.45)
+y = (display_height * 0.8)
+
+while not crashed:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			crashed = True
+
+	game_display.fill(white)
+	car(x, y)
+
+	pygame.display.update()
+	clock.tick(60)
+
+pygame.quit()
+quit()
