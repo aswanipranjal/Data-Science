072375a605b54565163fddb286dbd8c0b3bd42ce
AngryCracker <ads6may@gmail.com>
2018-01-16 12:07:05 +0530

    gene pool defined

--- Machine Learning/Aima/ga_sentence_gen.py
+++ Machine Learning/Aima/ga_sentence_gen.py
@@ -19,9 +19,7 @@ target = 'To be or not to be.'
 max_population = 500
 mutation_rate = 0.01
 # possible genes
-u_case = [chr(x) for x in range(65, 91)]
-l_case = [chr(x) for x in range(97, 123)]
-u_case.extend(l_case)
+gene_pool = [chr(x) for x in range(33, 127)]
 
 def loop():
 	crashed = False
@@ -34,7 +32,7 @@ def loop():
 		pygame.display.update()
 		clock.tick(60)
 
-print(u_case)
+print(gene_pool)
 loop()
 pygame.quit()
 quit()
