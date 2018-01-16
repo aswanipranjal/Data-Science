47bcb948e1fc2a6512d5f16a9259301aa51852bd
AngryCracker <ads6may@gmail.com>
2018-01-16 14:42:01 +0530

    minor

--- Machine Learning/Aima/ga_sentence_gen.py
+++ Machine Learning/Aima/ga_sentence_gen.py
@@ -17,7 +17,8 @@ clock = pygame.time.Clock()
 
 target = 'To be'
 max_population = 500
-mutation_rate = 0.01
+mutation_rate = 0.1
+f_thres = len(target)
 # possible genes
 gene_pool = [chr(x) for x in range(33, 127)]
 gene_pool.append(' ')
@@ -45,10 +46,11 @@ def loop():
 				pygame.quit()
 				return
 
-		population = [search.mutate(search.recombine(*search.select(2, population, fitness_fn)), gene_pool, 0.1)]
-		# fittest_individual
+		population = [search.mutate(search.recombine(*search.select(2, population, fitness_fn)), gene_pool, mutation_rate)]
+		fittest_individual = search.fitness_threshold(fitness_fn, f_thres, population)
+		current_best = ''.join(fittest_individual)
 		game_display.fill(white)
-		text_surface, text_rect = text_objects('Hello bitch!', large_text)
+		text_surface, text_rect = text_objects(current_best, large_text)
 		text_rect.center = ((display_width/2), (display_height/2))
 		game_display.blit(text_surface,  text_rect)
 		pygame.display.update()
