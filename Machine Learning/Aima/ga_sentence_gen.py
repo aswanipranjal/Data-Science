20af0a012c2ac1d71aabf53c55f1a2c627ee568a
AngryCracker <ads6may@gmail.com>
2018-01-16 14:39:03 +0530

    minor

--- Machine Learning/Aima/ga_sentence_gen.py
+++ Machine Learning/Aima/ga_sentence_gen.py
@@ -35,17 +35,25 @@ def fitness_fn(_list):
 	return fitness
 
 def loop():
-	crashed = False
+	# crashed = False
+	i = 0
 	large_text = pygame.font.Font('freesansbold.ttf', 115)
-	while not crashed:
+	first_population = search.init_population(max_population, gene_pool, len(target))
+	while i < 50:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				return
 
-		text_surface, text_rect = text_objects
+		population = [search.mutate(search.recombine(*search.select(2, population, fitness_fn)), gene_pool, 0.1)]
+		# fittest_individual
+		game_display.fill(white)
+		text_surface, text_rect = text_objects('Hello bitch!', large_text)
+		text_rect.center = ((display_width/2), (display_height/2))
+		game_display.blit(text_surface,  text_rect)
 		pygame.display.update()
 		clock.tick(60)
+		i += 1
 
 # print(gene_pool)
 # first_population = search.init_population(max_population, gene_pool, len(target))
@@ -68,7 +76,8 @@ def loop():
 # 	print(''.join(selections[i]))
 # 	print(fitness_fn(selections[i]))
 # # print(first_sample_phrase)
-first_population = search.init_population(max_population, gene_pool, len(target))
+
+# first_population = search.init_population(max_population, gene_pool, len(target))
 max_of_gen_1 = search.genetic_algorithm(first_population, fitness_fn, gene_pool, 5, 10)
 print(max_of_gen_1)
 loop()
