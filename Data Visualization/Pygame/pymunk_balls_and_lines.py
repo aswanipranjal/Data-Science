d33f191833a758cf6adae5d8ca6191f1af4b16df
AngryCracker <ads6may@gmail.com>
2018-01-18 12:32:05 +0530

    increase radius of clicked circle

--- Data Visualization/Pygame/pymunk_balls_and_lines.py
+++ Data Visualization/Pygame/pymunk_balls_and_lines.py
@@ -13,3 +13,11 @@ COLLTYPE_BALL = 2
 def flipy(y):
 	# converts chipmunk physics to pygame coordinates
 	return -y + 600
+
+def mouse_coll_func(arbiter, space, data):
+	# simple callback to increase the radius of circles touching the mouse
+	s1, s2 = arbiter.shapes
+	s2.unsafe_set_radius(s2.radius + 0.15)
+	return False
+
+	
