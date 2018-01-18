245d2876360b5c62cfca1947830cedde627311d9
AngryCracker <ads6may@gmail.com>
2018-01-18 06:23:25 +0530

    finding closest empty planets

--- Machine Learning/Halite2/MyBot.py
+++ Machine Learning/Halite2/MyBot.py
@@ -12,4 +12,10 @@ while True:
     for ship in game_map.get_me().all_ships():
         shipid = ship.id
         if ship.docking_status != ship.DockingStatus.UNDOCKED:
-            
+            continue
+
+        entities_by_distance = game_map.nearby_entities_by_distance(ship)
+        entities_by_distance = OrderedDict(sorted(entities_by_distance.items(), key=lambda t: t[0]))
+
+        closest_empty_planets = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned()]
+        
