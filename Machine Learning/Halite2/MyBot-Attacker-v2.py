36b147ca97c463ed9797f1c888009b818dab2b4a
AngryCracker <ads6may@gmail.com>
2018-01-19 07:11:06 +0530

    finding closest empty planets instead of enemy planets

--- Machine Learning/Halite2/MyBot-Attacker-v2.py
+++ Machine Learning/Halite2/MyBot-Attacker-v2.py
@@ -21,6 +21,7 @@ while True:
 		entities_by_distance = game_map.nearby_entities_by_distance(ship)
 		entities_by_distance = OrderedDict(sorted(entities_by_distance.items(), key=lambda t: t[0]))
 
-		closest_enemy_planets = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned()]
+		closest_empty_planets = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned()]
 		closest_enemy_ships = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Ship) and entities_by_distance[distance][0] not in team_ships]
-		
+
+		if len(cl)
