28ae231ccd666ba3b4245a8f83b4b2e68c1077e1
AngryCracker <ads6may@gmail.com>
2018-01-19 14:21:35 +0530

    fitness function defined

--- Machine Learning/Aima/ga_sentence_gen3.py
+++ Machine Learning/Aima/ga_sentence_gen3.py
@@ -11,3 +11,15 @@ u_case = [chr(x) for x in range(65, 91)]
 l_case = [chr(x) for x in range(97, 123)]
 gene_pool = u_case.extend(l_case)
 gene_pool.append(' ')
+
+def fitness_fn(_list):
+	fitness = 0
+	phrase = ''.join(_list)
+	for i in range(len(phrase)):
+		if target[i] == phrase[i]:
+			fitness += 1
+	return fitness
+
+def main():
+	population = search.init_population(max_population, gene_pool, len(target))
+	
