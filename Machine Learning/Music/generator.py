ca73e34c7ee01ba3824dcc0cb12cb7c9a085c8ad
AngryCracker <ads6may@gmail.com>
2018-01-19 11:27:41 +0530

    generator helper class

---
+++ Machine Learning/Music/generator.py
@@ -0,0 +1,68 @@
+from __future__ import print_function
+import sys
+
+from music21 import *
+import numpy as np
+
+from grammar import *
+from preprocess import *
+from qa import *
+import lstm
+
+def __sample(a, temperature=1.0):
+	a = np.log(a) / temperature
+	a = np.exp(a) / np.sum(np.exp(a))
+	return np.argmax(np.random.multinomial(1, a, 1))
+
+def __predict(model, x, indices_val, diversity):
+	preds = model.predict(x, verbose=0)[0]
+	next_index = __sample(preds, diversity)
+	next_val = indices_val[next_index]
+
+	return next_val
+
+def __generate_grammar(model, corpus, abstract_grammars, values, val_indices,
+                       indices_val, max_len, max_tries, diversity):
+    curr_grammar = ''
+    # np.random.randint is exclusive to high
+    start_index = np.random.randint(0, len(corpus) - max_len)
+    sentence = corpus[start_index: start_index + max_len]    # seed
+    running_length = 0.0
+    while running_length <= 4.1:    # arbitrary, from avg in input file
+        # transform sentence (previous sequence) to matrix
+        x = np.zeros((1, max_len, len(values)))
+        for t, val in enumerate(sentence):
+            if (not val in val_indices): print(val)
+            x[0, t, val_indices[val]] = 1.
+
+        next_val = __predict(model, x, indices_val, diversity)
+
+        # fix first note: must not have < > and not be a rest
+        if (running_length < 0.00001):
+            tries = 0
+            while (next_val.split(',')[0] == 'R' or 
+                len(next_val.split(',')) != 2):
+                # give up after 1000 tries; random from input's first notes
+                if tries >= max_tries:
+                    print('Gave up on first note generation after', max_tries, 
+                        'tries')
+                    # np.random is exclusive to high
+                    rand = np.random.randint(0, len(abstract_grammars))
+                    next_val = abstract_grammars[rand].split(' ')[0]
+                else:
+                    next_val = __predict(model, x, indices_val, diversity)
+
+                tries += 1
+
+        # shift sentence over with new value
+        sentence = sentence[1:] 
+        sentence.append(next_val)
+
+        # except for first case, add a ' ' separator
+        if (running_length > 0.00001): curr_grammar += ' '
+        curr_grammar += next_val
+
+        length = float(next_val.split(',')[1])
+        running_length += length
+
+    return curr_grammar
