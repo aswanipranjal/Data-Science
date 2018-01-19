e05f97f028cc52256b118ae7b6e8b196a86811ee
AngryCracker <ads6may@gmail.com>
2018-01-19 17:14:56 +0530

    defined box coordinates and rigidbody properties

--- Data Visualization/Pygame/pymunk_pyramid.py
+++ Data Visualization/Pygame/pymunk_pyramid.py
@@ -18,4 +18,27 @@ class Pyramid:
 		self.space.gravity = (0.0, -900.0)
 		self.space.sleep_time_threshold = 0.3
 
-		
+		shape = pymunk.Segment(self.space.static_body, (5, 100), (595, 100), 1.0)
+		shape.friction = 1.0
+		self.space.add(shape)
+
+		x = Vec2d(-270, 7.5) + (300, 100)
+		y = Vec2d(0, 0)
+		deltaX = Vec2d(0.5625, 1.1) * 20
+		deltaY = Vec2d(1.125, 0.0) * 20
+
+		for i in range(25):
+			y = Vec2d(x)
+			for j in range(i, 25):
+				size = 10
+				points = [(-size, -size), (-size, size), (size, size), (size, -size)]
+				mass = 1.0
+				moment = pymunk.moment_for_poly(mass, points, (0, 0))
+				body = pymunk.Body(mass, moment)
+				body.position = y
+				shape = pymunk.Poly(body, points)
+				shape.friction = 1
+				self.space.add(body, shape)
+
+				y += deltaY
+			x += deltaX
