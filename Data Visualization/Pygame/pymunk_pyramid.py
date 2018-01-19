946ae9c3d21eb49e2c7f00c926c1a62e4dc30075
AngryCracker <ads6may@gmail.com>
2018-01-19 17:08:33 +0530

    boilerplate

---
+++ Data Visualization/Pygame/pymunk_pyramid.py
@@ -0,0 +1,17 @@
+import pygame
+from pygame.locals import *
+from pygame.color import *
+
+import pymunk
+from pymunk import Vec2d
+import pymunk.pygame_util
+
+class Pyramid:
+	def __init__(self):
+		self.running = True
+		self.drawing = True
+		self.w, self.h = 600, 600
+		self.screen = pygame.display.set_mode((self.w, self.h))
+		self.clock = pygame.time.Clock()
+
+		
