a100aa11c107c210fe302bbf5bd37ef52c725a97
AngryCracker <ads6may@gmail.com>
2018-01-19 18:25:58 +0530

    defined rigidbody properties for balls

--- Data Visualization/Pygame/pymunk_contact_and_no_flipy.py
+++ Data Visualization/Pygame/pymunk_contact_and_no_flipy.py
@@ -43,4 +43,21 @@ def main():
 	while running:
 		for event in pygame.event.get():
 			if event.type == QUIT:
-				running = False			
+				running = False		
+			elif event.type == KEYDOWN and event.key == K_ESCAPE:
+				running = False
+			elif event.type == KEYDOWN and event.key == K_p:
+				pygame.image.save(screen, 'contact_and_no_flipy.png')
+
+		ticks_to_next_ball -= 1
+		if ticks_to_next_ball <= 0:
+			ticks_to_next_ball = 100
+			mass = 10
+			radius = 25
+			inertia = pm.moment_for_circle(mass, 0, radius, (0, 0))
+			body = pm.Body(mass, inertia)
+			x = random.randint(115, 350)
+			body.position = x, 200
+			shape = pm.Circle(body, radius, (0, 0))
+			space.add(body, shape)
+			balls.append(shape)
