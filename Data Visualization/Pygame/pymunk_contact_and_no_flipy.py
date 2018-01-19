acce7f36ad99a43cb23352afeb37a990fedd4627
AngryCracker <ads6may@gmail.com>
2018-01-19 18:04:20 +0530

    function that draws circles at collision points

--- Data Visualization/Pygame/pymunk_contact_and_no_flipy.py
+++ Data Visualization/Pygame/pymunk_contact_and_no_flipy.py
@@ -10,4 +10,7 @@ from pymunk import Vec2d
 
 def draw_collision(arbiter, space, data):
 	for c in arbiter.contact_point_set.points:
-		
+		r = max(3, abs(c.distance * 5))
+		r = int(r)
+		p = tuple(map(int, c.point_a))
+		pygame.draw.circle(data['surface'], THECOLORS['red'], p, r, 0)
