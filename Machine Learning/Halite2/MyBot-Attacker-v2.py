db7e7214d0fb64ebce7592ecbbfc7ba907f07688
AngryCracker <ads6may@gmail.com>
2018-01-19 07:28:02 +0530

    defined undefined variables

--- Machine Learning/Halite2/MyBot-Attacker-v2.py
+++ Machine Learning/Halite2/MyBot-Attacker-v2.py
@@ -9,6 +9,7 @@ while True:
 	game_map = game.update_map()
 	command_queue = []
 	team_ships = game_map.get_me().all_ships()
+	team_ships_main = team_ships
 	team_ships_extra = []
 	if len(team_ships) > 20:
 		team_ships_main = team_ships[:20]
