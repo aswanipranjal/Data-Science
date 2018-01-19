0668af25912e227b1d6c61ac3f27d6954be9252f
AngryCracker <ads6may@gmail.com>
2018-01-19 07:25:37 +0530

    extra ships are sent into attack mode

--- Machine Learning/Halite2/MyBot-Attacker-v2.py
+++ Machine Learning/Halite2/MyBot-Attacker-v2.py
@@ -33,4 +33,24 @@ while True:
 				if navigate_command:
 					command_queue.append(navigate_command)
 
-			
+		elif len(closest_enemy_ships) > 0:
+			target_ship = closest_enemy_ships[0]
+			navigate_command = ship.navigate(ship.closest_point_to(target_ship), game_map, speed=int(hlt.constants.MAX_SPEED), ignore_ships=False)
+			if navigate_command:
+				command_queue.append(navigate_command)
+
+	if len(team_ships_extra) > 0:
+		for ship in team_ships_extra:
+			shipid = ship.id
+
+			entities_by_distance = game_map.nearby_entities_by_distance(ship)
+			entities_by_distance = OrderedDict(sorted(entities_by_distance.items(), key=lambda t: t[0]))
+			closest_enemy_ships = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Ship) and entities_by_distance[distance][0] not in team_ships]
+
+			if len(closest_enemy_ships) > 0:
+				target_ship = closest_enemy_ships[0]
+				navigate_command = ship.navigate(ship.closest_point_to(target_ship), game_map, speed=int(hlt.constants.MAX_SPEED), ignore_ships=False)
+				if navigate_command:
+					command_queue.append(navigate_command)
+
+	game.send_command_queue(command_queue)
