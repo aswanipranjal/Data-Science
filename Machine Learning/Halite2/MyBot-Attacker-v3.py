8cb9b5ba446b237e330149dc61557d7a213166e2
AngryCracker <ads6may@gmail.com>
2018-01-19 07:53:57 +0530

    entities by distance calculated

--- Machine Learning/Halite2/MyBot-Attacker-v3.py
+++ Machine Learning/Halite2/MyBot-Attacker-v3.py
@@ -9,4 +9,11 @@ while True:
 	game_map = game.update_map()
 	command_queue = []
 	team_ships = game_map.get_me().all_ships()
-	
+	for ship in team_ships:
+		shipid = ship.id
+		if ship.docking_status != ship.DockingStatus.UNDOCKED:
+			continue
+
+		entities_by_distance = game_map.nearby_entities_by_distance(ship)
+		entities_by_distance = OrderedDict(sorted(entities_by_distance.itens(), key=lambda t: t[0]))
+		
