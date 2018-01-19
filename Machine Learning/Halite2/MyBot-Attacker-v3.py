d84fcc784cd0ff54c96db65e3970d25d21a90353
AngryCracker <ads6may@gmail.com>
2018-01-19 08:35:23 +0530

    minor

--- Machine Learning/Halite2/MyBot-Attacker-v3.py
+++ Machine Learning/Halite2/MyBot-Attacker-v3.py
@@ -14,7 +14,7 @@ while True:
 		if ship.docking_status != ship.DockingStatus.UNDOCKED:
 			continue
 		entities_by_distance = game_map.nearby_entities_by_distance(ship)
-		entities_by_distance = OrderedDict(sorted(entities_by_distance.itens(), key=lambda t: t[0]))
+		entities_by_distance = OrderedDict(sorted(entities_by_distance.items(), key=lambda t: t[0]))
 		closest_empty_planets = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned()]
 		closest_enemy_ships =   [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Ship) and entities_by_distance[distance][0] not in team_ships]
 		closest_empty_planet_distances = [distance for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned()]
@@ -53,4 +53,5 @@ while True:
 			navigate_command = ship.navigate(ship.closest_point_to(target_ship), game_map, speed=int(hlt.constants.MAX_SPEED), ignore_ships=False)
 			if navigate_command:
 				command_queue.append(navigate_command)
+
 	game.send_command_queue(command_queue)
