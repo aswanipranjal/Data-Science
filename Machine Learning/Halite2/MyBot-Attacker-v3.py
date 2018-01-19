481d947e406f9fa88fd4c178807a17df9725560f
AngryCracker <ads6may@gmail.com>
2018-01-19 07:56:49 +0530

    finding closest enemy ships

--- Machine Learning/Halite2/MyBot-Attacker-v3.py
+++ Machine Learning/Halite2/MyBot-Attacker-v3.py
@@ -18,4 +18,5 @@ while True:
 		entities_by_distance = OrderedDict(sorted(entities_by_distance.itens(), key=lambda t: t[0]))
 
 		closest_empty_planets = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned()]
+		closest_enemy_ships = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Ship) and entities_by_distance[distance][0] not in team_ships]
 		
