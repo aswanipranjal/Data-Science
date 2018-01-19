1171a95c6c4a7167dbfadba099dc05704a7d410a
AngryCracker <ads6may@gmail.com>
2018-01-19 17:02:28 +0530

    redux

--- Machine Learning/Aima/ga_sentence_gen3.py
+++ Machine Learning/Aima/ga_sentence_gen3.py
@@ -26,7 +26,6 @@ def genetic_algorithm_stepwise(population, fitness_fn, gene_pool=[0, 1], f_thres
 	for i in range(ngen):
 		population = [search.mutate(search.recombine(*search.select(2, population, fitness_fn)), gene_pool, pmut) for i in range(len(population))]
 		current_best = ''.join(argmax(population, key=fitness_fn))
-		# print('Current best: ' + current_best + '\t' + 'Iteration: ' + str(i) + '\r', end='')
 		print(f'Current best: {current_best}\tIteration: {str(i)}\tFitness: {fitness_fn(current_best)}\r', end='')
 
 		fittest_individual = search.fitness_threshold(fitness_fn, f_thres, population)
@@ -38,10 +37,6 @@ def genetic_algorithm_stepwise(population, fitness_fn, gene_pool=[0, 1], f_thres
 def main():
 	population = search.init_population(max_population, gene_pool, len(target))
 	solution, iterations = genetic_algorithm_stepwise(population, fitness_fn, f_thres=len(target), gene_pool=gene_pool, pmut=mutation_rate)
-	# print()
-	# print(''.join(solution))
-	# print(fitness_fn(solution))
-	# print(iterations)
 
 if __name__ == '__main__':
 	main()
