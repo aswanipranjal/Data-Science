15b9b54bac13115cbca4de0b35e1d3ea6db30a49
AngryCracker <ads6may@gmail.com>
2018-01-19 10:42:34 +0530

    imports

--- Machine Learning/Music/lstm.py
+++ Machine Learning/Music/lstm.py
@@ -1,1 +1,7 @@
 from __future__ import print_function
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM
+import numpy as np
+
