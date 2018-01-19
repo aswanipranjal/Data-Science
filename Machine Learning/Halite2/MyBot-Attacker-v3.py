2a8a831934d9b4578d23caa131a79c24de86e332
AngryCracker <ads6may@gmail.com>
2018-01-19 08:02:52 +0530

    calculated closest empty planet distances and closest enemy ship distances

--- Machine Learning/Halite2/MyBot-Attacker-v3.py
+++ Machine Learning/Halite2/MyBot-Attacker-v3.py
@@ -18,5 +18,9 @@ while True:
 		entities_by_distance = OrderedDict(sorted(entities_by_distance.itens(), key=lambda t: t[0]))
 
 		closest_empty_planets = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned()]
-		closest_enemy_ships = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Ship) and entities_by_distance[distance][0] not in team_ships]
-		
+		closest_enemy_ships =   [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Ship) and entities_by_distance[distance][0] not in team_ships]
+		closest_empty_planet_distances = [distance for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned()]
+		closest_enemy_ship_distances =   [distance for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Ship) and entities_by_distance[distance][0] not in team_ships]
+
+		if len(closest_empty_planets) > 0:
+			if len(closest_enemy_ships) > 0:
