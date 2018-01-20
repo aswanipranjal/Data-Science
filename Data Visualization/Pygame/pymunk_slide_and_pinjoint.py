7f7792a9a4bea775b1640ed36bcf251c0e552afa
AngryCracker <ads6may@gmail.com>
2018-01-20 09:46:11 +0530

    adds L-shape with two joints

--- Data Visualization/Pygame/pymunk_slide_and_pinjoint.py
+++ Data Visualization/Pygame/pymunk_slide_and_pinjoint.py
@@ -16,3 +16,22 @@ def add_ball(space):
 	shape = pymunk.Circle(body, radius, (0, 0))
 	space.add(body, shape)
 	return shape
+
+def add_L(space):
+	rotation_center_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+	rotation_center_body.position = (300, 300)
+
+	rotation_limit_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+	rotation_limit_body.position = (200, 300)
+
+	body = pymunk.Body(10, 10000)
+	body.position = (300, 300)
+	l1 = pymunk.Segment(body, (-150.0, 0), (255.0, 0.0), 1)
+	l2 = pymunk.Segment(body, (-150.0, 0), (-150.0, 50.0), 1)
+
+	rotation_center_joint = pymunk.PinJoint(body, rotation_center_body, (0, 0), (0, 0))
+	joint_limit = 25
+	rotation_limit_joint = pymunk.SlideJoint(body, rotation_limit_body, (-100, 0), (0, 0), 0, joint_limit)
+
+	space.add(l1, l2, body, rotation_center_joint, rotation_limit_joint)
+	return l1, l2
