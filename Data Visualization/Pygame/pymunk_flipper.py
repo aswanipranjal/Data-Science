c58c238528067a8a78b791f621efa56db041ec95
AngryCracker <ads6may@gmail.com>
2018-01-20 11:16:45 +0530

    pygame boilerplate

--- Data Visualization/Pygame/pymunk_flipper.py
+++ Data Visualization/Pygame/pymunk_flipper.py
@@ -9,3 +9,11 @@ from pymunk import Vec2d
 import pymunk.pygame_util
 
 pygame.init()
+screen = pygame.display.set_mode((600, 600))
+clock = pygame.time.Clock()
+running = True
+
+space = pymunk.Space()
+space.gravity = (0.0, -900.0)
+draw_options = pymunk.pygame_util.DrawOptions(screen)
+
