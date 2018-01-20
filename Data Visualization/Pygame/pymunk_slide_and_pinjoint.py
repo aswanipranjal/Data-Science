edad14e63454146b7c8bf8c33c7c6cbde2af027d
AngryCracker <ads6may@gmail.com>
2018-01-20 09:41:53 +0530

    helper function to add a ball

--- Data Visualization/Pygame/pymunk_slide_and_pinjoint.py
+++ Data Visualization/Pygame/pymunk_slide_and_pinjoint.py
@@ -7,3 +7,12 @@ import pymunk
 import pymunk.pygame_util
 
 def add_ball(space):
+	mass = 1
+	radius = 14
+	inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
+	body = pymunk.Body(mass, inertia)
+	x = random.randint(120, 380)
+	body.position = x, 550
+	shape = pymunk.Circle(body, radius, (0, 0))
+	space.add(body, shape)
+	return shape
