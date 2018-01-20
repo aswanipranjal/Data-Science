5ea580b637fb22b3c83710541c45093db894bc55
AngryCracker <ads6may@gmail.com>
2018-01-20 06:48:59 +0530

    rigidbody physics defined

--- Data Visualization/Pygame/pymunk_breakout.py
+++ Data Visualization/Pygame/pymunk_breakout.py
@@ -19,3 +19,14 @@ collision_types = {
 	'bottom': 3,
 	'player': 4,
 }
+
+def spawn_ball(space, position, direction):
+	ball_body = pymunk.Body(1, pymunk.inf)
+	ball_body.position = position
+
+	ball_shape = pymunk.Circle(ball_body, 5)
+	ball_shape.color = THECOLORS['green']
+	ball_shape.elasticity = 1.0
+	ball_shape.collision_type = collision_types['ball']
+
+	ball_body.apply_impulse_at_local_point(Vec2d(direction))
