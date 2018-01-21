3155e0d999de7fad1108bf80d260ce4af9f671ad
AngryCracker <ads6may@gmail.com>
2018-01-21 17:48:19 +0530

    global physics variables

--- Data Visualization/Pygame/pymunk_spiderweb.py
+++ Data Visualization/Pygame/pymunk_spiderweb.py
@@ -10,3 +10,8 @@ from pymunk.vec2d import Vec2d
 config = pyglet.gl.Config(sample_buffers=1, samples=2, double_buffer=True)
 window = pyglet.window.Window(config=config, vsync=False)
 space = pymunk.Space()
+
+space.gravity = 0, -900
+space.damping = .999
+c = Vec2d(window.width / 2., window.height / 2.)
+
