e8bbfcc07213f1f68251b2914e300a5e87eee620
AngryCracker <ads6may@gmail.com>
2018-01-21 11:26:17 +0530

    function to run the genetic algorithm

--- Machine Learning/Aima/pygame_sentence_ga.py
+++ Machine Learning/Aima/pygame_sentence_ga.py
@@ -64,3 +64,14 @@ def fitness_fn(_list):
 			fitness += 1
 	return fitness
 
+def genetic_algorithm_stepwise(population, fitness_fn, gene_pool=[0, 1], f_thres=None, ngen=1200, pmut=0.1):
+	for i in range(ngen):
+		population = [search.mutate(search.recombine(*search.select(2, population, fitness_fn)), gene_pool, pmut) for i in range(len(population))]
+		current_best = ''.join(argmax(population, key=fitness_fn))
+		print(f'current best: {current_best}\tIteration: {str(i)}\tFitness: {fitness_fn(current_best)}\r', end='')
+
+		fittest_individual = search.fitness_threshold(fitness_fn, f_thres, population)
+		if fittest_individual:
+			return fittest_individual, i
+
+	return argmax(population, key=fitness_fn), i
