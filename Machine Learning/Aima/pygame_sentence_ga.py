d25bb6835e2d057c23ec50a68dd0a9cb8ae23713
AngryCracker <ads6may@gmail.com>
2018-01-21 11:40:33 +0530

    debug

--- Machine Learning/Aima/pygame_sentence_ga.py
+++ Machine Learning/Aima/pygame_sentence_ga.py
@@ -1,21 +1,21 @@
-import time
-import pygame
-import random
+# import time
+# import pygame
+# import random
 
 import search
 import random
 from utils import argmax
 
-pygame.init()
-display_width = 800
-display_height = 600
+# pygame.init()
+# display_width = 800
+# display_height = 600
 
-black = (0, 0, 0)
-white = (255, 255, 255)
+# black = (0, 0, 0)
+# white = (255, 255, 255)
 
-screen = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('Genetic Algorithm')
-clock = pygame.time.Clock()
+# screen = pygame.display.set_mode((display_width, display_height))
+# pygame.display.set_caption('Genetic Algorithm')
+# clock = pygame.time.Clock()
 
 # genetic algorithm variables
 target = ''
@@ -36,25 +36,25 @@ gene_pool.extend(u_case)
 gene_pool.extend(l_case)
 gene_pool.append(' ')
 
-def text_objects(text, font):
-	text_surface = font.render(text, True, black)
-	return text_surface, text_surface.get_rect()
+# def text_objects(text, font):
+# 	text_surface = font.render(text, True, black)
+# 	return text_surface, text_surface.get_rect()
 
-def button(msg, x, y, w, h, i_color, a_color, action=None):
-	mouse = pygame.mouse.get_pos()
-	click = pygame.mouse.get_pressed()
+# def button(msg, x, y, w, h, i_color, a_color, action=None):
+# 	mouse = pygame.mouse.get_pos()
+# 	click = pygame.mouse.get_pressed()
 
-	if x + w > mouse[0] > x and y + h > mouse[1] > y:
-		pygame.draw.rect(screen, a_color, (x, y, w, h))
-		if click[0] == 1 and action != None:
-			action()
-	else:
-		pygame.draw.rect(screen, i_color, (x, y, w, h))
+# 	if x + w > mouse[0] > x and y + h > mouse[1] > y:
+# 		pygame.draw.rect(screen, a_color, (x, y, w, h))
+# 		if click[0] == 1 and action != None:
+# 			action()
+# 	else:
+# 		pygame.draw.rect(screen, i_color, (x, y, w, h))
 
-	small_text = pygame.font.Font('freesansbold.ttf', 20)
-	m_text_surface, m_text_rect = text_objects(msg, small_text)
-	m_text_rect.center = ((x + (w / 2)), (y + (h / 2)))
-	screen.blit(m_text_surface, m_text_rect)
+# 	small_text = pygame.font.Font('freesansbold.ttf', 20)
+# 	m_text_surface, m_text_rect = text_objects(msg, small_text)
+# 	m_text_rect.center = ((x + (w / 2)), (y + (h / 2)))
+# 	screen.blit(m_text_surface, m_text_rect)
 
 def fitness_fn(_list):
 	fitness = 0
