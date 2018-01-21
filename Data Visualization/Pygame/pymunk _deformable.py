68fb75e3e9d3694268f98eecdcd42b37269e73bf
AngryCracker <ads6may@gmail.com>
2018-01-21 08:06:28 +0530

    helper function to draw help text

--- Data Visualization/Pygame/pymunk _deformable.py
+++ Data Visualization/Pygame/pymunk _deformable.py
@@ -10,3 +10,15 @@ import pymunk
 from pymunk import Vec2d, BB
 import pymunk.pygame_util
 import pymunk.autogeometry
+
+def draw_helptext(screen):
+	font = pygame.font.Font(None, 16)
+	text = ['LMB (hold): Draw pink color',
+			'Shift + LMB (hold): Create balls',
+			'g: Generate segments from pink color drawing',
+			'r: Reset',]
+	y = 5
+	for line in text:
+		text = font.render(line, 1, THECOLORS['black'])
+		screen.blit(text, (5, y))
+		y += 10
