f7065084597436831450fbc98ab0b6bc6d29c5db
AngryCracker <ads6may@gmail.com>
2018-01-21 09:58:43 +0530

    pygame boilerplate variables

--- Machine Learning/Aima/pygame_sentence_ga.py
+++ Machine Learning/Aima/pygame_sentence_ga.py
@@ -5,3 +5,14 @@ import random
 import search
 import random
 from utils import argmax
+
+pygame.init()
+display_width = 800
+display_height = 600
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+screen = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('Genetic Algorithm')
+clock = pygame.time.Clock()
