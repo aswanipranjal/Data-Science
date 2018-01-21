cf36cfcfa132f889b71bf5d109d7e3cecb37c945
AngryCracker <ads6may@gmail.com>
2018-01-21 17:55:43 +0530

    defined bodies

--- Data Visualization/Pygame/pymunk_spiderweb.py
+++ Data Visualization/Pygame/pymunk_spiderweb.py
@@ -26,3 +26,26 @@ s = pymunk.Circle(cb, 15)
 s.filter = pymunk.ShapeFilter(group=web_group)
 s.ignore_draw = True
 space.add(cb, s)
+
+for x in range(0, 101):
+	b = pymunk.Body(1, 1)
+	v = Vec2d.unit()
+	v.angle_degrees = x * 18
+	scale = window.height / 2. / 6. * .5
+
+	dist += 1 / 18.
+	dist = dist ** 1.005
+
+	offset = 0
+	offset = [0.0, -0.80, -1.0, -0.80][((x*18) % 360) // 18 % 4]
+	offset = .8 + offset
+	offset *= dist ** 2.8 / 100.
+
+	v.length = scale * (dist + offset)
+
+	b.position = c + v
+	s = pymunk.Circle(b, 15)
+	s.filter = pymunk.ShapeFilter(group=web_group)
+	s.ignore_draw = True
+	space.add(b, s)
+	bs.append(b)
