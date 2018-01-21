fc5f2e1173417eb85b58b16e40e2e742d9dc046a
AngryCracker <ads6may@gmail.com>
2018-01-21 11:46:24 +0530

    uncommented pygame boilerplate

--- Machine Learning/Aima/pygame_sentence_ga.py
+++ Machine Learning/Aima/pygame_sentence_ga.py
@@ -1,21 +1,21 @@
-# import time
-# import pygame
-# import random
+import time
+import pygame
+import random
 
 import search
 import random
 from utils import argmax
 
-# pygame.init()
-# display_width = 800
-# display_height = 600
+pygame.init()
+display_width = 800
+display_height = 600
 
-# black = (0, 0, 0)
-# white = (255, 255, 255)
+black = (0, 0, 0)
+white = (255, 255, 255)
 
-# screen = pygame.display.set_mode((display_width, display_height))
-# pygame.display.set_caption('Genetic Algorithm')
-# clock = pygame.time.Clock()
+screen = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption('Genetic Algorithm')
+clock = pygame.time.Clock()
 
 # genetic algorithm variables
 target = 'Machine Learning'
@@ -25,10 +25,10 @@ f_thres = len(target)
 
 u_case = [chr(x) for x in range(65, 91)]
 l_case = [chr(x) for x in range(97, 123)]
-# punctuations1 = [chr(x) for x in range(33, 48)]
-# punctuations2 = [chr(x) for x in range(58, 65)]
-# punctuations3 = [chr(x) for x in range(91, 97)]
-# numerals = [chr(x) for x in range(48, 58)]
+punctuations1 = [chr(x) for x in range(33, 48)]
+punctuations2 = [chr(x) for x in range(58, 65)]
+punctuations3 = [chr(x) for x in range(91, 97)]
+numerals = [chr(x) for x in range(48, 58)]
 
 # extend the gene pool with the required lists
 gene_pool = []
