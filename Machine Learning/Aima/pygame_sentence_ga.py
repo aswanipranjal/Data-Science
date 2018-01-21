5ba374e52218eba1878c3931495db97b8c8c1cc5
AngryCracker <ads6may@gmail.com>
2018-01-21 16:33:15 +0530

    loading bar gradient

--- Machine Learning/Aima/pygame_sentence_ga.py
+++ Machine Learning/Aima/pygame_sentence_ga.py
@@ -100,8 +100,9 @@ def game_loop(population, fitness_fn, gene_pool=[0, 1], f_thres=None, ngen=1200,
 			screen.blit(m_text_surface2, m_text_rect2)
 			screen.blit(m_text_surface3, m_text_rect3)
 
+		scaling_factor = fitness_fn(current_best) / len(target)
 		pygame.draw.rect(screen, p_blue, (m_text_rect[0], m_text_rect[1] + 85, m_text_rect[2], 10), 2)
-		pygame.draw.rect(screen, (12, 57, 76), (m_text_rect[0], m_text_rect[1] + 85, m_text_rect[2] * fitness_fn(current_best) / len(target), 10))
+		pygame.draw.rect(screen, (12, 57, 76 + 40 * scaling_factor), (m_text_rect[0], m_text_rect[1] + 85, m_text_rect[2] * scaling_factor, 10))
 
 		fittest_individual = search.fitness_threshold(fitness_fn, f_thres, population)
 		if fittest_individual:
