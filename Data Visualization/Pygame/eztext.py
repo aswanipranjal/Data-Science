a42981d9617e92acb2c9e248c8e11d8d6517fab9
AngryCracker <ads6may@gmail.com>
2018-01-23 07:21:38 +0530

    defining keystroke functions

--- Data Visualization/Pygame/eztext.py
+++ Data Visualization/Pygame/eztext.py
@@ -1,3 +1,19 @@
 import pygame
 from pygame.locals import *
 
+def name():
+	pygame.init()
+	screen = pygame.display.set_mode((480, 360))
+	name = ''
+	font = pygame.font.Font(None, 50)
+	while True:
+		for event in pygame.event.get():
+			if event.type == KEYDOWN:
+				if event.unicode.isalpha():
+					name += event.unicode
+				elif event.key == K_BACKSPACE:
+					name = name[:-1]
+				elif event.key == K_RETURN:
+					name = ''
+			elif event.type == QUIT:
+				return
