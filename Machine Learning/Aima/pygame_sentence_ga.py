eb4a6457a20f4da474845261f49f64676ea1ba8d
AngryCracker <ads6may@gmail.com>
2018-01-23 06:24:48 +0530

    minor

--- Machine Learning/Aima/pygame_sentence_ga.py
+++ Machine Learning/Aima/pygame_sentence_ga.py
@@ -138,7 +138,7 @@ def game_loop(population, fitness_fn, gene_pool=[0, 1], f_thres=None, ngen=1200,
 			screen.blit(g_text_surface, g_text_rect)
 
 		else:
-			button('Next', display_width * 0.9, display_height * 0.9, display_width * 0.1, display_height * 0.1, p_blue, light_p_blue, NOne)
+			button('Next', display_width * 0.9, display_height * 0.9, display_width * 0.1, display_height * 0.1, p_blue, light_p_blue, None)
 
 		# updates the screen
 		pygame.display.update()
