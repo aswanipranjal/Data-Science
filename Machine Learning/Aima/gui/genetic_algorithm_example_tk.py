e2c1af177c7c4092b9cf1a5a2a4f2cfbe8c9e3a3
AngryCracker <ads6may@gmail.com>
2018-01-26 18:36:43 +0530

    tk boilerplate

--- Machine Learning/Aima/gui/genetic_algorithm_example_tk.py
+++ Machine Learning/Aima/gui/genetic_algorithm_example_tk.py
@@ -35,8 +35,21 @@ class GeneticAlgorithm(tk.Tk):
 class HomeScreen(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
-		label = ttk.Label(self, text='Start Page', font=LARGE_FONT)
+		label = ttk.Label(self, text='Home Screen', font=LARGE_FONT)
 		label.pack(pady=10, padx=10)
 
-		button1 = ttk.Button(self, text='Visit Page 1', command=lambda: placeholder_function())
+		button1 = ttk.Button(self, text='Visit Run Screen', command=lambda: placeholder_function())
 		button1.pack()
+
+class RunScreen(tk.Frame):
+	def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+		label = ttk.Label(self, text='Run Screen', font=LARGE_FONT)
+		label.pack(pady=10, padx=10)
+
+		button1 = ttk.Button(self, text='Visit Home Screen', command=lambda: placeholder_function())
+		button1.pack()
+
+app = GeneticAlgorithm()
+app.geometry('800x600')
+app.mainloop()
