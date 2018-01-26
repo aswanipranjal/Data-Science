89a30dc2356fe5e1bfc8b2b6ff0bca1af1a9611d
AngryCracker <ads6may@gmail.com>
2018-01-26 09:29:08 +0530

    adding frames

--- Data Visualization/GUI/tk12.py
+++ Data Visualization/GUI/tk12.py
@@ -3,3 +3,10 @@ import tkinter as tk
 class SeaofBTCapp(tk.Tk):
 	def __init__(self, *args, **kwargs):
 		tk.Tk.__init__(self, *args, **kwargs)
+		container = tk.Frame(self)
+		container.pack(side='TOP', fill='BOTH', expand=True)
+		container.grid_rowconfigure(0, weight=1)
+		container.grid_columnconfigure(0, weight=1)
+		
+		self.frames = {}
+		frame = start_page(container, self)
