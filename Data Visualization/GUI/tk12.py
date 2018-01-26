4971c17b2e8f6491c0554b644ca38d2a27bfc73b
AngryCracker <ads6may@gmail.com>
2018-01-26 11:58:10 +0530

    replace tk with ttk  for widgets

--- Data Visualization/GUI/tk12.py
+++ Data Visualization/GUI/tk12.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 LARGE_FONT = ('Verdana', 12)
 
@@ -26,31 +27,31 @@ class SeaofBTCapp(tk.Tk):
 class StartPage(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
-		label = tk.Label(self, text='Start Page', font=LARGE_FONT)
+		label = ttk.Label(self, text='Start Page', font=LARGE_FONT)
 		label.pack(pady=10, padx=10)
 
-		button1 = tk.Button(self, text='Visit Page 1', command=lambda: controller.show_frame(PageOne))
+		button1 = ttk.Button(self, text='Visit Page 1', command=lambda: controller.show_frame(PageOne))
 		button1.pack()
 
-		button2 = tk.Button(self, text='Visit Page 2', command=lambda: controller.show_frame(PageTwo))
+		button2 = ttk.Button(self, text='Visit Page 2', command=lambda: controller.show_frame(PageTwo))
 		button2.pack()
 
 class PageOne(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
-		label = tk.Label(self, text='Page One', font=LARGE_FONT)
+		label = ttk.Label(self, text='Page One', font=LARGE_FONT)
 		label.pack(pady=10, padx=10)
-		button1 = tk.Button(self, text='Back to home', command=lambda: controller.show_frame(StartPage))
+		button1 = ttk.Button(self, text='Back to home', command=lambda: controller.show_frame(StartPage))
 		button1.pack()
-		button2 = tk.Button(self, text='Visit Page 2', command=lambda: controller.show_frame(PageTwo))
+		button2 = ttk.Button(self, text='Visit Page 2', command=lambda: controller.show_frame(PageTwo))
 		button2.pack()
 
 class PageTwo(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
-		label = tk.Label(self, text='Page Two', font=LARGE_FONT)
+		label = ttk.Label(self, text='Page Two', font=LARGE_FONT)
 		label.pack(pady=10, padx=10)
-		button = tk.Button(self, text='Back to home', command=lambda: controller.show_frame(StartPage))
+		button = ttk.Button(self, text='Back to home', command=lambda: controller.show_frame(StartPage))
 		button.pack()
 
 app = SeaofBTCapp()
