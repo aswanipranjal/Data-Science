4b6baeda2da5aadc53868031bc1e0c2a67e83316
AngryCracker <ads6may@gmail.com>
2018-01-26 11:45:51 +0530

    adding F frame classes in self.frames

--- Data Visualization/GUI/tk12.py
+++ Data Visualization/GUI/tk12.py
@@ -11,9 +11,12 @@ class SeaofBTCapp(tk.Tk):
 		container.grid_columnconfigure(0, weight=1)
 		
 		self.frames = {}
-		frame = StartPage(container, self)
-		self.frames[StartPage] = frame
-		frame.grid(row=0, column=0, sticky='nsew')
+
+		for F in (StartPage, PageOne):
+			frame = F(container, self)
+			self.frames[F] = frame
+			frame.grid(row=0, column=0, sticky='nsew')
+		
 		self.show_frame(StartPage)
 
 	def show_frame(self, controller):
