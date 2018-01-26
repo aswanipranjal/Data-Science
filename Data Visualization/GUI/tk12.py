a69532848b2011ddaeb46135a17f04753a08beaa
AngryCracker <ads6may@gmail.com>
2018-01-26 16:53:50 +0530

    refactor

--- Data Visualization/GUI/tk12.py
+++ Data Visualization/GUI/tk12.py
@@ -22,7 +22,7 @@ def popupmsg(text):
 	popup = tk.Tk()
 	popup.wm_title('!')
 	label = ttk.Label(popup, text=text, font=NORMAL_FONT)
-	label.pack(side=tk.TOP, fill=tk.X, pady=10)
+	label.pack(side='top', fill='x', pady=10)
 	button1 = ttk.Button(popup, text='OK', command=popup.destroy)
 	button1.pack()
 	popup.mainloop()
