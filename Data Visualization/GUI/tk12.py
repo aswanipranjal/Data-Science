02663e058cbc00d3c0cf9fecc52648c7b90ecfda
AngryCracker <ads6may@gmail.com>
2018-01-26 09:41:41 +0530

    fixed _tkinter.TclError and NameError

--- Data Visualization/GUI/tk12.py
+++ Data Visualization/GUI/tk12.py
@@ -6,7 +6,7 @@ class SeaofBTCapp(tk.Tk):
 	def __init__(self, *args, **kwargs):
 		tk.Tk.__init__(self, *args, **kwargs)
 		container = tk.Frame(self)
-		container.pack(side='TOP', fill='BOTH', expand=True)
+		container.pack(side='top', fill='both', expand=True)
 		container.grid_rowconfigure(0, weight=1)
 		container.grid_columnconfigure(0, weight=1)
 		
@@ -22,7 +22,7 @@ class SeaofBTCapp(tk.Tk):
 
 class StartPage(tk.Frame):
 	def __init__(self, parent, controller):
-		tf.Frame.__init__(self, parent)
+		tk.Frame.__init__(self, parent)
 		label = tk.Label(self, text='Start Page', font=LARGE_FONT)
 		label.pack(pady=10, padx=10)
 
