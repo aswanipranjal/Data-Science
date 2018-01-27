0e72c46752b6754407cc982379be4dcd267d4abf
AngryCracker <ads6may@gmail.com>
2018-01-27 14:15:06 +0530

    mousedrag bound

--- Data Visualization/GUI/tk16.py
+++ Data Visualization/GUI/tk16.py
@@ -8,3 +8,9 @@ def paint(event):
 	x1, y1 = (event.x - 1), (event.y - 1)
 	x2, y2 = (event.x + 1), (event.y + 1)
 	w.create_oval(x1, y1, x2, y2, fill=python_green)
+
+root = Tk()
+root.title('Paint')
+w = Canvas(root, width=canvas_width, height=canvas_height)
+w.pack(expand=YES, fill=BOTH)
+w.bind('<B1-Motion>', paint)
