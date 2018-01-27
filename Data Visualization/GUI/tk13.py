11dbc39220c07c993343284a0e857b94261d305c
AngryCracker <ads6may@gmail.com>
2018-01-27 10:53:03 +0530

    update_idletasks

---
+++ Data Visualization/GUI/tk13.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from time import sleep
+
+root = Tk()
+var = StringVar()
+var.set('Hello')
+
+l = Label(root, textvariable=var)
+l.pack()
+
+for i in range(6):
+	sleep(1)
+	var.set('Goodbye', if i%2 else 'Hello')
+	root.update_idletasks()
