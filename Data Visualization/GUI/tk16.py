2f6490ae24b2b2b026f6d3095659c9c5d4db77c8
AngryCracker <ads6may@gmail.com>
2018-01-27 14:11:54 +0530

    function to draw ovals

--- Data Visualization/GUI/tk16.py
+++ Data Visualization/GUI/tk16.py
@@ -3,3 +3,8 @@ from tkinter import *
 canvas_width = 500
 canvas_height = 150
 
+def paint(event):
+	python_green = '#476042'
+	x1, y1 = (event.x - 1), (event.y - 1)
+	x2, y2 = (event.x + 1), (event.y + 1)
+	w.create_oval(x1, y1, x2, y2, fill=python_green)
