ad7ea75d56183f6382a6dc2a9f409b92221d7e31
AngryCracker <ads6may@gmail.com>
2018-01-28 13:57:25 +0530

    inconsistent tabs and spaces

--- Machine Learning/Aima/gui/genetic_algorithm_example_tk_2.py
+++ Machine Learning/Aima/gui/genetic_algorithm_example_tk_2.py
@@ -66,10 +66,10 @@ class ValidatingEntry(Entry):
 	def __init__(self, master, value='', **kw):
 		apply(Entry.__init__, (self, master), kw)
 		self.__value = value
-        self.__variable = StringVar()
-        self.__variable.set(value)
-        self.__variable.trace("w", self.__callback)
-        self.config(textvariable=self.__variable)
+		self.__variable = StringVar()
+		self.__variable.set(value)
+		self.__variable.trace('w', self.__callback)
+		self.config(textvariable=self.__variable)
 
     def __callback(self, *dummy):
     	value = self.__variable.get()
