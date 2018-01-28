7635fb6ce857f9394652d09f3bb834c20e26a751
AngryCracker <ads6may@gmail.com>
2018-01-28 13:12:59 +0530

    setting default value of entry widget on home screen

--- Machine Learning/Aima/gui/genetic_algorithm_example_tk_2.py
+++ Machine Learning/Aima/gui/genetic_algorithm_example_tk_2.py
@@ -92,6 +92,7 @@ for frame in (f1, f2):
 # Home Screen (f1) widgets
 button = ttk.Button(f1, text='RUN', command=lambda: raise_frame(f2, init=True)).pack(side=BOTTOM, pady=50)
 target_entry = ttk.Entry(f1, font=('Consolas 46 bold'), exportselection=0, foreground=p_blue, justify=CENTER)
+target_entry.insert(0, target)
 target_entry.pack(expand=YES, side=TOP, fill=X, padx=50)
 max_population_slider = Scale(f1, from_=3, to=1000, orient=HORIZONTAL, label='Max population', command=lambda value: update_max_population(int(value)))
 max_population_slider.set(max_population)
