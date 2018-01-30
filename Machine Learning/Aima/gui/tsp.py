df21a6a3436be5c8fe2dfcc38a34195f96e98df4
AngryCracker <ads6may@gmail.com>
2018-01-30 15:01:44 +0530

    TSP_problem subclass

--- Machine Learning/Aima/gui/tsp.py
+++ Machine Learning/Aima/gui/tsp.py
@@ -7,3 +7,16 @@ from search import *
 import numpy as np
 
 distances = {}
+
+class TSP_problem(Problem):
+
+	def two_opt(self, state):
+		neighbour_state = state[:]
+		left = random.randint(0, len(neighbour_state) - 1)
+		right = random.randint(0, len(neighbour_state) - 1)
+		if left > right:
+			left, right = right, left
+		neighbour_state[left: right + 1] = reversed(neighbour_state[left: right + 1])
+		return neighbour_state
+
+	
