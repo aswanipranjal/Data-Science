1b8532c42d86ddc822b3e741ca4a76b716672078
AngryCracker <ads6may@gmail.com>
2018-02-01 17:54:56 +0530

    fixed NameError

--- Machine Learning/Aima/gui/tsp_placeholder.py
+++ Machine Learning/Aima/gui/tsp_placeholder.py
@@ -225,7 +225,7 @@ class TSP_Gui():
         population = init_population(100, current.state, len(current.state))
         all_time_best = current.state
         while(1):
-            population = [mutate(racombine(*select(2, population, fitness_fn)), mutation_rate) for i in range(len(population))]
+            population = [mutate(recombine(*select(2, population, fitness_fn)), mutation_rate) for i in range(len(population))]
             current_best = utils.argmax(population, key=fitness_fn)
             if fitness_fn(current_best) > fitness_fn(all_time_best):
                 all_time_best = current_best
