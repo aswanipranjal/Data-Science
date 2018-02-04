2db91c7d3da9072dc8177a3b2e2272d0e373f6b4
AngryCracker <ads6may@gmail.com>
2018-02-04 19:32:06 +0530

    MDPapp class

--- Machine Learning/Aima/gui/mdp.py
+++ Machine Learning/Aima/gui/mdp.py
@@ -1,2 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
+
+class MDPapp(tk.Tk):
+
+	def __init__(self, *args, **kwargs):
+		tk.Tk.__init__(self, *args, **kwargs)
+		tk.Tk.wm_title(self, 'Grid MDP')
