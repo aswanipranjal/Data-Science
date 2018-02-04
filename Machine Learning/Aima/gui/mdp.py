12abe15c155de10afdcf745f5adec5493a13af73
AngryCracker <ads6may@gmail.com>
2018-02-04 19:33:58 +0530

    configuration

--- Machine Learning/Aima/gui/mdp.py
+++ Machine Learning/Aima/gui/mdp.py
@@ -6,3 +6,7 @@ class MDPapp(tk.Tk):
 	def __init__(self, *args, **kwargs):
 		tk.Tk.__init__(self, *args, **kwargs)
 		tk.Tk.wm_title(self, 'Grid MDP')
+		container = tk.Frame(self)
+		container.pack(side='top', fill='both', expand=True)
+		container.grid_rowconfigure(0, weight=1)
+		container.grid_columnconfigure(0, weight=1)
