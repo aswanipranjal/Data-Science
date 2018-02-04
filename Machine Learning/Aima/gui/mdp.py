474fe4d493a316ea267d17b4944dcc2ee6473116
AngryCracker <ads6may@gmail.com>
2018-02-04 19:55:16 +0530

    fixed AttributeError

--- Machine Learning/Aima/gui/mdp.py
+++ Machine Learning/Aima/gui/mdp.py
@@ -16,7 +16,7 @@ class MDPapp(tk.Tk):
 		for F in (HomePage, BuildMDP):
 			frame = F(container, self)
 			self.frames[F] = frame
-			frame.gird(row=0, column=0, sticky='nsew')
+			frame.grid(row=0, column=0, sticky='nsew')
 
 		self.show_frame(HomePage)
 
