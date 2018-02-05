3ffa662c047e16bd5ac01e1119b7b3b10ea10efa
AngryCracker <ads6may@gmail.com>
2018-02-05 23:42:05 +0530

    minor

--- Machine Learning/Aima/gui/mdp.py
+++ Machine Learning/Aima/gui/mdp.py
@@ -65,10 +65,9 @@ class BuildMDP(tk.Frame):
 	def create_buttons(self):
 		_height = self.controller.shared_data['height'].get()
 		_width = self.controller.shared_data['width'].get()
-		w = int(196/_width)
 		for i in range(_height):
 			for j in range(_width):
-				ttk.Button(self, text=f'{i}, {j}', width=w).grid(row=i, column=j)
+				ttk.Button(self, text=f'{i}, {j}', width=int(196/_width)).grid(row=i, column=j)
 
 app = MDPapp()
 app.geometry('1280x720')
