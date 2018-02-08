06e16da051c63b66da91fbf8ff0dac22f44bc7bf
AngryCracker <ads6may@gmail.com>
2018-02-08 16:22:30 +0530

    partial call to initialize reset all

--- Machine Learning/Aima/gui/mdp.py
+++ Machine Learning/Aima/gui/mdp.py
@@ -34,7 +34,7 @@ def initialize_dialogbox(_width, _height, gridmdp, terminals, buttons):
 	# widget_disability_checks
 	btn_apply = ttk.Button(container, text='Apply', command=partial(initialize_update_table, _width, _height, gridmdp, terminals, buttons, reward, term, wall, label_reward, entry_reward, rbtn_term, rbtn_wall))
 	btn_apply.grid(row=5, column=0, sticky='nsew', pady=5, padx=5)
-	btn_reset = ttk.Button(container, text='Reset')
+	btn_reset = ttk.Button(container, text='Reset', command=partial(initialize_reset_all, _width, _height, gridmdp, terminals, buttons, label_reward, entry_reward, rbtn_wall, rbtn_term))
 	btn_reset.grid(row=5, column=1, sticky='nsew', pady=5, padx=5)
 	btn_ok = ttk.Button(container, text='Ok', command=dialog.destroy)
 	btn_ok.grid(row=5, column=2, sticky='nsew', pady=5, padx=5)
