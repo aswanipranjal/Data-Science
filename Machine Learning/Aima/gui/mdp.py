d2535dbe31a7567621c57e1987dbfb09a216ecf4
AngryCracker <ads6may@gmail.com>
2018-02-09 05:13:19 +0530

    building boolean masks

--- Machine Learning/Aima/gui/mdp.py
+++ Machine Learning/Aima/gui/mdp.py
@@ -122,8 +122,8 @@ def widget_disability_checks(i, j, gridmdp, terminals, label_reward, entry_rewar
 # Todo: fix this function to do something useful
 def initialize_widget_disability_checks(_width, _height, gridmdp, terminals, label_reward, entry_reward, rbtn_wall, rbtn_term):
 	
-	bool_walls = [[False]*max(1, _width) for _ in range(max(1, _height))]
-	bool_terms = [[False]*max(1, _width) for _ in range(max(1, _height))]
+	bool_walls_mask = [[0]*max(1, _width) for _ in range(max(1, _height))]
+	bool_terms_mask = [[0]*max(1, _width) for _ in range(max(1, _height))]
 	for i in range(1, _height):
 		for j in range(1, _width):
 			if gridmdp[i][j] == WALL_VALUE:
