c61166f4a37cfeb929c03801b48f0b057e82600f
AngryCracker <ads6may@gmail.com>
2018-02-09 19:41:41 +0530

    -fixed SyntaxError -reset terminated flag

--- Machine Learning/Aima/gui/grid_mdp.py
+++ Machine Learning/Aima/gui/grid_mdp.py
@@ -520,7 +520,8 @@ class SolveMDP(tk.Frame):
 		for k, v in U.items():
 			self.grid_to_show[k[1]][k[0]] = v
 
-		if self.delta < self.epsilon * (1 - self.gamma) / self.gamma and self.terminated = False:
+		if self.delta < self.epsilon * (1 - self.gamma) / self.gamma and self.terminated == False:
+			self.terminated = True
 			display(self.grid_to_show)
 		
 		ax = fig.gca()
