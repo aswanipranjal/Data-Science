2b148785342b310ff2fd67bf15a0fc2af6437558
AngryCracker <ads6may@gmail.com>
2018-02-09 14:13:32 +0530

    move function call to App class

--- Machine Learning/Aima/gui/mdp.py
+++ Machine Learning/Aima/gui/mdp.py
@@ -277,6 +277,8 @@ class MDPapp(tk.Tk):
 		self.frames[SolveMDP] = frame
 		frame.grid(row=0, column=0, sticky='nsew')
 		self.show_frame(SolveMDP)
+		solve_page = self.get_page(SolveMDP)
+		solve_page.create_graph()
 
 	def show_frame(self, controller, cb=False):
 
