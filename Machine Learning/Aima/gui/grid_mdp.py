8377e84a6d57e52412024795564e070a6bcf03aa
AngryCracker <ads6may@gmail.com>
2018-02-09 20:51:46 +0530

    flipud

--- Machine Learning/Aima/gui/grid_mdp.py
+++ Machine Learning/Aima/gui/grid_mdp.py
@@ -45,15 +45,10 @@ def display(gridmdp, _height, _width):
 
 	container = tk.Frame(dialog)
 	container.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-	# container.grid_rowconfigure(0, weight=1)
-	# container.grid_columnconfigure(0, weight=1)
-
-	# label = ttk.Label(container, text='Values', font=('Helvetica', 8))
-	# label.grid(row=0, column=0, columnspan=_width)
-
+	
 	for i in range(max(1, _height)):
 		for j in range(max(1, _width)):
-			label = ttk.Label(container, text=f'{gridmdp[i][j]:.2f}', font=('Helvetica', 12))
+			label = ttk.Label(container, text=f'{gridmdp[_height - i - 1][j]:.2f}', font=('Helvetica', 12))
 			label.grid(row=i + 1, column=j + 1, padx=3, pady=3)
 
 	dialog.mainloop()
