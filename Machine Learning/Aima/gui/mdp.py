0bd1d4194889bf1c10f0a037b1a9cb927c8bad7c
AngryCracker <ads6may@gmail.com>
2018-02-09 13:52:43 +0530

    animating through build function gives AtrtributeError: 'NoneType' object has no attribute new_timer()

--- Machine Learning/Aima/gui/mdp.py
+++ Machine Learning/Aima/gui/mdp.py
@@ -293,8 +293,6 @@ class MDPapp(tk.Tk):
 		self.frames[SolveMDP] = frame
 		frame.grid(row=0, column=0, sticky='nsew')
 		self.show_frame(SolveMDP)
-		solve_page = self.get_page(SolveMDP)
-		solve_page.build()
 
 	def show_frame(self, controller, cb=False):
 
@@ -380,9 +378,6 @@ class SolveMDP(tk.Frame):
 
 		tk.Frame.__init__(self, parent)
 		self.controller = controller
-
-	def build(self):
-
 		self.controller.menu_bar.entryconfig('Build', state=tk.NORMAL)
 		canvas = FigureCanvasTkAgg(fig, self)
 		canvas.show()
