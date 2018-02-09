08ab2e334beb934cb36fef5d97b953ab68a31270
AngryCracker <ads6may@gmail.com>
2018-02-09 14:17:19 +0530

    animating heatmap

--- Machine Learning/Aima/gui/mdp.py
+++ Machine Learning/Aima/gui/mdp.py
@@ -381,18 +381,21 @@ class SolveMDP(tk.Frame):
 
 	def animate_graph(self, i):
 
-		pull_data = open('C:/Users/Aman Deep Singh/Documents/Python/Data Science/Data Visualization/GUI/sampledata.txt', 'r').read()
-		data_list = pull_data.split('\n')
-		x_list = []
-		y_list = []
-		for line in data_list:
-			if len(line) > 1:
-				x, y = line.split(',')
-				x_list.append(int(x))
-				y_list.append(int(y))
+		# pull_data = open('C:/Users/Aman Deep Singh/Documents/Python/Data Science/Data Visualization/GUI/sampledata.txt', 'r').read()
+		# data_list = pull_data.split('\n')
+		# x_list = []
+		# y_list = []
+		# for line in data_list:
+		# 	if len(line) > 1:
+		# 		x, y = line.split(',')
+		# 		x_list.append(int(x))
+		# 		y_list.append(int(y))
 
+		# sub.clear()
+		# sub.plot(x_list, y_list)
+		a = np.random.randn((16, 16))
 		sub.clear()
-		sub.plot(x_list, y_list)
+		sub.imshow(a, cmap='Oranges', interpolation='nearest')
 
 app = MDPapp()
 app.geometry('1280x720')
