7418549804449d4144d9c71e79264da0e1f14f04
AngryCracker <ads6may@gmail.com>
2018-02-11 15:54:14 +0530

    third assertion

--- Machine Learning/Aima/test_mdp.py
+++ Machine Learning/Aima/test_mdp.py
@@ -49,6 +49,8 @@ def test_policy_iteration():
         (2, 1): (0, 1), (2, 2): (1, 0), (3, 0): (-1, 0),
         (3, 1): None, (3, 2): None}
 
+    assert policy_iteration(sequential_decision_environment_2) == {(0, 1): (0, 1), (1, 2): (1, 0), (3, 2): None, (0, 0): (1, 0), (3, 0): (0, 1), (3, 1): None, (2, 1): (1, 0), (2, 0): (1, 0), (2, 2): (1, 0), (1, 0): (1, 0), (0, 2): (1, 0)}
+
 
 def test_best_policy():
     pi = best_policy(sequential_decision_environment,
