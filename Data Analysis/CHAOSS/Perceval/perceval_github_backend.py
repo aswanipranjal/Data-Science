baed3d52c8f73f3b0091b5ad3409857470a13763
Aman Deep Singh <ads6may@gmail.com>
2018-03-01 01:53:58 +0530

    print item

--- Data Analysis/CHAOSS/Perceval/perceval_github_backend.py
+++ Data Analysis/CHAOSS/Perceval/perceval_github_backend.py
@@ -6,3 +6,13 @@ parser = argparse.ArgumentParser(description='Simple parser for GitHub issues an
 parser.add_argument('-t', '--token', help='GitHub token')
 parser.add_argument('-r', '--repo', help='GitHub repository, as \'owner/repo\'')
 args = parser.parse_args()
+
+(owner, repo) = args.repo.split('/')
+repo = GitHub(owner=owner, repository=repo, api_token=args.token)
+
+for item in repo.fetch():
+	if 'pull_request' in item['data']:
+		kind = 'Pull Request'
+	else:
+		kind = 'Issue'
+	print(f'{item['data']['number']} : {kind}')
