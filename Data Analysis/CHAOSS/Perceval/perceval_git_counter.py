a2d673aaffc02f845abdbc4a895a900ca4060512
Aman Deep Singh <ads6may@gmail.com>
2018-03-01 00:58:44 +0530

    count commits

--- Data Analysis/CHAOSS/Perceval/perceval_git_counter.py
+++ Data Analysis/CHAOSS/Perceval/perceval_git_counter.py
@@ -5,3 +5,13 @@ parser = argparse.ArgumentParser(description='Count commits in a git repository'
 parser.add_argument('repo', help='Repository url')
 parser.add_argument('dir', help='Directory for cloning the repository')
 parser.add_argument('--print', action='store_true', help='Print hashes')
+args = parser.parse_args()
+
+repo = Git(uri=args.repo, gitpath=args.dir)
+count = 0
+
+for commit in repo.fetch():
+	if args.print:
+		print(commit['data']['commit'])
+	count += 1
+print(f'Number of commits: {count}')
