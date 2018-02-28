13edd0db9823b9509c32c1a54bcb1d0f04aaa591
Aman Deep Singh <ads6may@gmail.com>
2018-03-01 00:25:20 +0530

    fetch commits

--- Data Analysis/CHAOSS/Perceval/perceval_test.py
+++ Data Analysis/CHAOSS/Perceval/perceval_test.py
@@ -2,3 +2,9 @@ from perceval.backends.core.git import Git
 
 repo_url = 'http://github.com/aimacode/aima-python.git'
 repo_dir = '/tmp/aima-python.git'
+
+repo = Git(uri=repo_url, gitpath=repo_dir)
+
+# fetch all commits as an iterator, and iterate it printing each hash
+for commit in repo.fetch():
+	print(commit['data']['commit'])
