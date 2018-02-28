3c7fd69536f24fa0311444637a258664ade09de3
Aman Deep Singh <ads6may@gmail.com>
2018-02-28 16:19:37 +0530

    generator class

--- Data Analysis/CHAOSS/generators.py
+++ Data Analysis/CHAOSS/generators.py
@@ -5,4 +5,22 @@ def simple_firstn(n):
 		num += 1
 	return nums
 
-sum_of_firstn = sum(simple_firstn(1000000))
+# using the generator pattern
+class firstn(object):
+
+	def __init__(self, n):
+		self.n = n;
+		self.num, self.nums = 0, []
+
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+		return self.next()
+
+	def next(self):
+		if self.num < self.n:
+			cur, self.num = self.num, self.num+1
+			return cur
+		else:
+			raise StopIteration()
