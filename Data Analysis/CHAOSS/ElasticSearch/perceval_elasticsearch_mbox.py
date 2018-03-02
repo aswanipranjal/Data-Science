988a2ba47d6d5c1d648ee9bdfe0cb69dc24dbd0a
Aman Deep Singh <ads6may@gmail.com>
2018-03-02 23:25:10 +0530

    minor

--- Data Analysis/CHAOSS/ElasticSearch/perceval_elasticsearch_mbox.py
+++ Data Analysis/CHAOSS/ElasticSearch/perceval_elasticsearch_mbox.py
@@ -14,4 +14,14 @@ except elasticsearch.exception.RequestError:
 	print('Index already exists, remove it before running this script again.')
 	exit()
 
-	
+repo = MBox(uri=mbox_uri, dirpath=mbox_dir)
+
+print('Analysing mbox archives')
+for message in repo.fetch():
+	summary = {
+		'from': message['data']['From'],
+		'subject': message['data']['Subject'],
+		'date': email.utils.parsedate_to_datetime(message['data']['Date'])
+	}
+	print('.', end='')
+	es.index(index='message', doc_type='summary', body=summary)
