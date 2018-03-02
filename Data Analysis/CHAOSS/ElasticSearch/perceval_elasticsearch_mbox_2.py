c80facf6ad81603d262ab0ad93dac0d2b7c7cb7f
Aman Deep Singh <ads6may@gmail.com>
2018-03-02 23:38:22 +0530

    minor

--- Data Analysis/CHAOSS/ElasticSearch/perceval_elasticsearch_mbox_2.py
+++ Data Analysis/CHAOSS/ElasticSearch/perceval_elasticsearch_mbox_2.py
@@ -5,6 +5,6 @@ es = elasticsearch.Elasticsearch(['http://localhost:9200/'])
 
 es_result = es.search(index='messages')
 
-print(f'Found {es_result['hits']['total']} messages')
+print(f"Found {es_result['hits']['total']} messages")
 for message in es_result['hits']['hits']:
-	print(f'Sender: {message['_source']['from']}\nSubject: {message['_source']['subject']}')
+	print(f"Sender: {message['_source']['from']}\nSubject: {message['_source']['subject']}")
