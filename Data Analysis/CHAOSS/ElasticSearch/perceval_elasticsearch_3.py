0badc2ab735813939b84042d90199e215352f7a1
Aman Deep Singh <ads6may@gmail.com>
2018-03-02 22:01:39 +0530

    minor

--- Data Analysis/CHAOSS/ElasticSearch/perceval_elasticsearch_3.py
+++ Data Analysis/CHAOSS/ElasticSearch/perceval_elasticsearch_3.py
@@ -10,3 +10,24 @@ repo_dir = '/tmp/perceval.git'
 es = elasticsearch.Elasticsearch(['http://localhost:9200/'])
 
 try:
+	es.indices.create('commits')
+except elasticsearch.exceptions.RequestError:
+	print('Index already exists, remove it before running this script again')
+	exit()
+
+repo = Git(uri=repo_url, gitpath=repo_dir)
+
+print('Analysing git repo')	
+for commit in repo.fetch():
+	summary = {
+		'hash': commit['data']['commit'],
+		'author': commit['data']['Author'],
+		'author_date': commit['data']['AuthorDate'],
+		'commit': commit['data']['Commit']
+		'commit_date': commit['data']['CommitDate'],
+		'files_no': len(commit['data']['files'])
+	}
+	print('.', end='')
+	es.index(index='commits', doc_type='summary', body=summary)
+
+print('\nCreated new index with commits')
