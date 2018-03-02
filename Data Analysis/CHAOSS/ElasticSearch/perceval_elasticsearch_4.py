5d1fce761fec0ebb8eaf684f014e003843e43655
Aman Deep Singh <ads6may@gmail.com>
2018-03-02 23:06:23 +0530

    minor

--- Data Analysis/CHAOSS/ElasticSearch/perceval_elasticsearch_4.py
+++ Data Analysis/CHAOSS/ElasticSearch/perceval_elasticsearch_4.py
@@ -3,30 +3,37 @@ import datetime
 from perceval.backends.core.git import Git
 import elasticsearch
 
+# Url for the git repo to analyze
 repo_url = 'http://github.com/grimoirelab/perceval.git'
+# Directory for letting Perceval clone the git repo
 repo_dir = '/tmp/perceval.git'
-
+# ElasticSearch instance (url)
 es = elasticsearch.Elasticsearch(['http://localhost:9200/'])
 
+# Create the 'commits' index in ElasticSearch
 try:
-	es.indices.create('commits')
+    es.indices.create('commits')
 except elasticsearch.exceptions.RequestError:
-	print('Index already exists, remove it before running this script again.')
-	exit()
-
+    print('Index already exisits, remove it before running this script again.')
+    exit()
+# Create a Git object, pointing to repo_url, using repo_dir for cloning
 repo = Git(uri=repo_url, gitpath=repo_dir)
-
-print('Analysing the repo')
+# Fetch all commits as an iteratoir, and iterate it uploading to ElasticSearch
+print('Analyzing git repo...')
 for commit in repo.fetch():
-	summary = {
-		'hash': commit['data']['commit'],
-		'author': commit['data']['Author'],
-		'author_date': datetime.datetime.strptime(commit['data']['AuthorDate'], "%a %b %d %H:%M:%S %Y %z"),
-		'commit': commit['data']['Commit'],
-		'commit_date': datetime.datetime.strptime(commit['data']['CommitDate'], "%a %b %d %H:%M:%S %Y %z"),
-		'files_no': len(commit['data']['files'])
-	}
-	print('.', end='')
-	es.index(index='commits', doc_type='summary', body=summary)
+    # Create the object (dictionary) to upload to ElasticSearch
+    summary = {
+        'hash': commit['data']['commit'],
+        'author': commit['data']['Author'],
+        'author_date': datetime.datetime.strptime(commit['data']['AuthorDate'],
+                                                "%a %b %d %H:%M:%S %Y %z"),
+        'commit': commit['data']['Commit'],
+        'commit_date': datetime.datetime.strptime(commit['data']['CommitDate'],
+                                                "%a %b %d %H:%M:%S %Y %z"),
+        'files_no': len(commit['data']['files'])
+        }
+    print('.', end='')
+    # Upload the object to ElasticSearch
+    es.index(index='commits', doc_type='summary', body=summary)
 
 print('\nCreated new index with commits.')
