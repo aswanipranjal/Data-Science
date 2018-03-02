0ad651f68eddf2b38d511e803437d2c9395b1c39
Aman Deep Singh <ads6may@gmail.com>
2018-03-02 20:40:18 +0530

    minor

--- Data Analysis/CHAOSS/ElasticSearch/perceval_elasticsearch_2.py
+++ Data Analysis/CHAOSS/ElasticSearch/perceval_elasticsearch_2.py
@@ -1,23 +1,26 @@
 from perceval.backends.core.git import Git
 import elasticsearch
 
-repo_url = 'https://github.com/grimoirelab/perceval.git'
+# Url for the git repo to analyze
+repo_url = 'http://github.com/grimoirelab/perceval.git'
+# Directory for letting Perceval clone the git repo
 repo_dir = '/tmp/perceval.git'
+# ElasticSearch instance (url)
+es = elasticsearch.Elasticsearch(['http://localhost:9200/'])
 
-es = elasticsearch.Elasticsearch(['https://localhost:9200/'])
-
-# create the 'commits' index in ElasticSearch
-# if it already exists, first delete it and then create it
+# Create the 'commits' index in ElasticSearch
+# if it already exisits, first delete it and then create it.
 try:
-	es.indices.create('commits')
+    es.indices.create('commits')
 except elasticsearch.exceptions.RequestError:
-	es.indices.delete('commits')
-	es.indices.create('commits')
-
+    es.indices.delete('commits')
+    es.indices.create('commits')
+# Create a Git object, pointing to repo_url, using repo_dir for cloning
 repo = Git(uri=repo_url, gitpath=repo_dir)
-
+# Fetch all commits as an iteratoir, and iterate it uploading to ElasticSearch
 for commit in repo.fetch():
-	summary = {'hash': commits['data']['commit']}
-	print(summary)
-
-	es.index(index='commits', doc_type='summary', body=summary)
+    # Create the object (dictionary) to upload to ElasticSearch
+    summary = {'hash': commit['data']['commit']}
+    print(summary)
+    # Upload the object to ElasticSearch
+es.index(index='commits', doc_type='summary', body=summary)
