195f8221291359bf5be8e5de87067419ade6e873
Aman Deep Singh <ads6may@gmail.com>
2018-03-03 21:57:25 +0530

    minor

--- Data Analysis/CHAOSS/ElasticSearch/data_collection.py
+++ Data Analysis/CHAOSS/ElasticSearch/data_collection.py
@@ -5,12 +5,6 @@ token = argv[1]
 repos = ['perceval', 'arthur', 'grimoireelk', 'sortinghat', 'mordred', 'panels', 'training']
 
 for repo in repos:
-	subprocess.run(['p2o.py', '--enrich', '--index', 'git_raw',
-      '--index-enrich', 'git', '-e', 'http://localhost:9200',
-      '--no_inc', '--debug', 'git',
-      'https://github.com/grimoirelab/' + repo])
+	subprocess.run(['p2o.py', '--enrich', '--index', 'git_raw', '--index-enrich', 'git', '-e', 'http://localhost:9200', '--no_inc', '--debug', 'git', 'https://github.com/grimoirelab/' + repo])
     # Produce github and github_raw indexes from GitHub issues and prs
-    subprocess.run(['p2o.py', '--enrich', '--index', 'github_raw',
-      '--index-enrich', 'github', '-e', 'http://localhost:9200',
-      '--no_inc', '--debug', 'github', 'grimoirelab', repo,
-      '-t', token, '--sleep-for-rate'])
+    subprocess.run(['p2o.py', '--enrich', '--index', 'github_raw', '--index-enrich', 'github', '-e', 'http://localhost:9200', '--no_inc', '--debug', 'github', 'grimoirelab', repo, '-t', token, '--sleep-for-rate'])
