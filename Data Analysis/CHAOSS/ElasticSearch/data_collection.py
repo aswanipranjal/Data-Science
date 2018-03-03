fa052c2c1db5f0f50e34c7c25788638b4a32b8e9
Aman Deep Singh <ads6may@gmail.com>
2018-03-04 00:00:34 +0530

    microtask 1

--- Data Analysis/CHAOSS/ElasticSearch/data_collection.py
+++ Data Analysis/CHAOSS/ElasticSearch/data_collection.py
@@ -6,3 +6,4 @@ repos = ['perceval', 'arthur', 'grimoireelk', 'sortinghat', 'mordred', 'panels',
 
 for repo in repos:
 	subprocess.run(['p2o.py', '--enrich', '--index', 'git_raw', '--index-enrich', 'git', '-e', 'http://localhost:9200', '--no_inc', '--debug', 'git', 'http://github.com/grimoirelab/' + repo])
+	subprocess.run(['p2o.py', '--enrich', '--index', 'github_raw', '--index-enrich', 'github', '-e', 'http://localhost_9200', '--no_inc', '--debug', 'github', 'grimoirelab', repo, '-t', token, '--sleep-for-rate'])
