a82e3e39c3897a5c470baccf65f895e418e1baac
AngryCracker <ads6may@gmail.com>
2018-03-09 17:17:08 +0530

    minor

--- Machine Learning/Aima/nqueensproblem_plot.py
+++ Machine Learning/Aima/nqueensproblem_plot.py
@@ -6,13 +6,13 @@ def plot_NQP(node):
 	board = np.zeros((n, n))
 	solution = list(node.state)
 	for i in range(n):
-		board[i][solution[i]] = 1
+		board[i][solution[i]] = True
 
-	fig = plt.figure(figsize=(6, 3.2))
+	fig = plt.figure(figsize=(10, 10))
 	ax = fig.add_subplot(111)
 	ax.set_title(f'{n} Queens')
 	plt.imshow(board, cmap='Oranges', interpolation='nearest')
 	ax.set_aspect('equal')
-	plt.colorbar(orientation='vertical')
+	fig.tight_layout()
 	plt.show()
 	plt.show()
