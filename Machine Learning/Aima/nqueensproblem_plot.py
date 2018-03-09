1bf6fdc2b6330dc6dc8391101781c54cedc7c483
AngryCracker <ads6may@gmail.com>
2018-03-09 17:10:41 +0530

    minor

--- Machine Learning/Aima/nqueensproblem_plot.py
+++ Machine Learning/Aima/nqueensproblem_plot.py
@@ -7,5 +7,17 @@ def plot_NQP(node):
 	solution = list(node.state)
 	for i in range(n):
 		board[i][solution[i]] = 1
+
+	fig = plt.figure(figsize=(6, 3.2))
+	ax = fig.add_subplot(111)
+	ax.set_title('colorMap')
 	plt.imshow(board, interpolation='nearest')
+	ax.set_aspect('equal')
+	cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
+	cax.get_xaxis().set_visible(False)
+	cax.get_yaxis().set_visible(False)
+	cax.patch.set_alpha(0)
+	cax.set_frame_on(False)
+	plt.colorbar(orientation='vertical')
+	plt.show()
 	plt.show()
