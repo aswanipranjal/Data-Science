de1afc81cddb1a5f792ae35eeac2be8247f155c7
AngryCracker <ads6may@gmail.com>
2018-03-12 23:45:08 +0530

    todo

---
+++ Machine Learning/Aima/todo.py
@@ -0,0 +1,27 @@
+'''
+1. Fix merge conflicts in logic.ipynb (if any)
+2. Convert %psource to psource in logic.ipynb
+3.a Add section for `KB_Agent` in logic.ipynb
+  b Add section for `FOL-FC-Ask` in logic.ipynb
+  c Add section for `FOL-BC-Ask` in logic.ipynb
+4. Write tests for dpll, 
+				dpll_satisfiable, 
+				WalkSAT, 
+				pl_resolution, 
+				unify,
+				tt_entails,
+				fix spelling of entailment in line 225 `test_logic.py`
+				Add space to line 235 in test_logic.py
+5. Add Simulated Annealing to search.ipynb
+6. One PR: Fix NQueensProblem, 
+	rewrite NQueensProblem section, 
+	add visualization of NQueensProblem to utils.py
+	rewrite EightPuzzle section,
+	add tests for NQueensProblem
+7. Add min_conflicts section to csp.ipynb
+8. Add section for SATplan to logic.ipynb
+9. POMDP (very difficult)
+10. POMDP value iteration
+11. HybridWumpusAgent (if incomplete)
+12. DLS, IDS, RBFS
+'''
