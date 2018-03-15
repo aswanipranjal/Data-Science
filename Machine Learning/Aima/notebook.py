42dabd6c22ec5b4f276c62d90d27bc7bf53bf560
AngryCracker <ads6may@gmail.com>
2018-03-15 11:44:53 +0530

    plotting function

--- Machine Learning/Aima/notebook.py
+++ Machine Learning/Aima/notebook.py
@@ -886,3 +886,31 @@ class Canvas_fol_bc_ask(Canvas):
         self.fill(0, 0, 0)
         self.text_n(self.table[self.context[0]][self.context[1]] if self.context else "Click for text", 0.025, 0.975)
         self.update()
+
+
+# Function to plot NQueensCSP in csp.py and NQueensProblem in search.py
+def plot_NQueens(solution):
+    n = len(solution)
+    board = np.array([2 * int((i + j) % 2) for j in range(n) for i in range(n)]).reshape((n, n))        
+    im = Image.open('images/queen_s.png')
+    height = im.size[1]
+    im = np.array(im).astype(np.float) / 255
+    fig = plt.figure(figsize=(7, 7))
+    ax = fig.add_subplot(111)
+    ax.set_title(f'{n} Queens')
+    plt.imshow(board, cmap='binary', interpolation='nearest')
+    # NQueensCSP gives a solution as a dictionary
+    if isinstance(solution, dict):
+        for (k, v) in solution.items():
+            newax = fig.add_axes([0.064 + (k * 0.112), 0.062 + ((7 - v) * 0.112), 0.1, 0.1], zorder=1)
+            newax.imshow(im)
+            newax.axis('off')
+    # NQueensProblem gives a solution as a list
+    elif isinstance(solution, list):
+        for (k, v) in enumerate(solution):
+            newax = fig.add_axes([0.064 + (k * 0.112), 0.062 + ((7 - v) * 0.112), 0.1, 0.1], zorder=1)
+            newax.imshow(im)
+            newax.axis('off')
+    fig.tight_layout()
+    plt.show()
+    
