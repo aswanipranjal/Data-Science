b7038b890fbd487883a98ddac6ba1d1f1f00a6e4
AngryCracker <ads6may@gmail.com>
2018-03-15 14:24:54 +0530

    initial state changed

--- Machine Learning/Aima/search.py
+++ Machine Learning/Aima/search.py
@@ -1120,8 +1120,8 @@ class NQueensProblem(Problem):
 
     def __init__(self, N):
         self.N = N
-        self.initial = [None] * N
-        Problem.__init__(self, tuple(self.initial))
+        self.initial = tuple([None] * N)
+        Problem.__init__(self, self.initial)
 
     def actions(self, state):
         """In the leftmost empty column, try all non-conflicting rows."""
