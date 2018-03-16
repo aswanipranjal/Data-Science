a1b01956b1dbbcab3478e889cf18f8b9cbbb43d5
AngryCracker <ads6may@gmail.com>
2018-03-16 20:45:42 +0530

    eightpuzzle

---
+++ Machine Learning/Aima/gui/eight_puzzle.py
@@ -0,0 +1,6 @@
+from tkinter import *
+import sys
+import os.path
+import math
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from search import astar_search, EightPuzzle
--- Machine Learning/Aima/todo.py
+++ Machine Learning/Aima/todo.py
@@ -3,14 +3,14 @@
 3.a Add section for `KB_Agent` in logic.ipynb
   b Add section for `FOL-FC-Ask` in logic.ipynb
   c Add section for `FOL-BC-Ask` in logic.ipynb
-4. Write tests for [dpll, ]
-				[dpll_satisfiable, ]
-				[WalkSAT, ]
-				[pl_resolution, ]
-				[unify,]
-				[tt_entails,]
-				[fix spelling of entailment in line 225 `test_logic.py`]
-				[Add space to line 235 in test_logic.py]
+4. Write tests for [[dpll, ]]
+				[[dpll_satisfiable, ]]
+				[[WalkSAT, ]]
+				[[pl_resolution, ]]
+				[[unify,]]
+				[[tt_entails,]]
+				[[fix spelling of entailment in line 225 `test_logic.py`]]
+				[[Add space to line 235 in test_logic.py]]
 5. Add Simulated Annealing to search.ipynb
 6. APPEND
 7. ANGELIC-SEARCH
