03f797369e145cb088dd9caf6e291a2d50debe3b
AngryCracker <ads6may@gmail.com>
2018-03-16 20:48:42 +0530

    imports

--- Machine Learning/Aima/gui/eight_puzzle.py
+++ Machine Learning/Aima/gui/eight_puzzle.py
@@ -1,6 +1,14 @@
-from tkinter import *
+# author: ad71
+import tkinter as tk
+import tkinter.messagebox
+from tkinter import ttk
+
+from functools import partial
+
 import sys
 import os.path
-import math
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 from search import astar_search, EightPuzzle
+import utils
+
