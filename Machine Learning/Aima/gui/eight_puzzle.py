a90ab9bd2b06b04642b2a834b5aa9b1312c57124
AngryCracker <ads6may@gmail.com>
2018-03-16 20:51:53 +0530

    container boilerplate

--- Machine Learning/Aima/gui/eight_puzzle.py
+++ Machine Learning/Aima/gui/eight_puzzle.py
@@ -12,3 +12,21 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from search import astar_search, EightPuzzle
 import utils
 
+class EightPuzzleApp(tk.Tk):
+
+	def __init__(self, *args, **kwargs):
+
+		tk.Tk.__init__(self, *args, **kwargs)
+		tk.Tk.wm_title(self, 'Eight Puzzle')
+		self.shared_data = {
+			'n': 8
+		}
+		self.shared_data['n'].set(8)
+		self.container = tk.Frame(self)
+		self.container.pack(side='top', fill='both', expand=True)
+		self.container.grid_rowconfigure(0, weight=1)
+		self.container.grid_columnconfigure(0, weight=1)
+
+		self.frames = {}
+
+		
