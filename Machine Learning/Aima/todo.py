5fe5f7b4cd695a32f88ed2195629768176919dfa
AngryCracker <ads6may@gmail.com>
2018-03-16 12:24:04 +0530

    more tests

--- Machine Learning/Aima/todo.py
+++ Machine Learning/Aima/todo.py
@@ -3,21 +3,17 @@
 3.a Add section for `KB_Agent` in logic.ipynb
   b Add section for `FOL-FC-Ask` in logic.ipynb
   c Add section for `FOL-BC-Ask` in logic.ipynb
-4. Write tests for dpll, 
-				dpll_satisfiable, 
-				WalkSAT, 
-				pl_resolution, 
-				unify,
-				tt_entails,
-				fix spelling of entailment in line 225 `test_logic.py`
-				Add space to line 235 in test_logic.py
+4. Write tests for [dpll, ]
+				[dpll_satisfiable, ]
+				[WalkSAT, ]
+				[pl_resolution, ]
+				[unify,]
+				[tt_entails,]
+				[fix spelling of entailment in line 225 `test_logic.py`]
+				[Add space to line 235 in test_logic.py]
 5. Add Simulated Annealing to search.ipynb
-6. One PR: 
-	[[Fix NQueensProblem, ]]
-	[[rewrite NQueensProblem section, ]]
-	[[add visualization of NQueensProblem to utils.py]]
-	[[rewrite EightPuzzle section,]]
-	[[add tests for NQueensProblem]]
+6. APPEND
+7. ANGELIC-SEARCH
 8. Add section for SATplan to logic.ipynb
 9. POMDP (very difficult)
 10. POMDP value iteration
