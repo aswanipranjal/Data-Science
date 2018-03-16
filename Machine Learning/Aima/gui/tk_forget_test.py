f3dbfc254cd682d5937d9ba3ee821380b1030b3f
AngryCracker <ads6may@gmail.com>
2018-03-16 22:29:25 +0530

    minor

--- Machine Learning/Aima/gui/tk_forget_test.py
+++ Machine Learning/Aima/gui/tk_forget_test.py
@@ -1,8 +1,14 @@
 from tkinter import *
 
+import numpy as np
+
 root = Tk()
 
-b = Button(root, text='Delete Me', command=lambda: b.pack_forget())
-b.pack()
+state = np.array([1, 2, 3, 4, 5, 6, 7, 8, 0])
+grid = state.reshape((3, 3))
+for i in range(3):
+	for j in range(3):
+		b = Button(root, text=f'{grid[i][j]}', command=lambda: b.pack_forget())
+		b.grid(row=i, column=j)
 
 root.mainloop()
