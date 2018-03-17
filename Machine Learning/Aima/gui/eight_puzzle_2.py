2a918284de5934672bc6e089e469002629ba078f
AngryCracker <ads6may@gmail.com>
2018-03-17 23:25:05 +0530

    minor

--- Machine Learning/Aima/gui/eight_puzzle_2.py
+++ Machine Learning/Aima/gui/eight_puzzle_2.py
@@ -1,5 +1,4 @@
 from tkinter import *
-# from tkinter import 
 from functools import partial
 
 import numpy as np
@@ -19,31 +18,31 @@ def exchange(index):
 	print('Zero: ', zero_ix)
 	# b[zero_ix] = b[index]
 	b[zero_ix].grid_forget()
-	b[zero_ix] = Button(root, text=f'{state[index]}', width=13, font=('Helvetica', 20, 'bold'), command=partial(exchange, zero_ix))
+	b[zero_ix] = Button(root, text=f'{state[index]}', width=13, font=('Helvetica', 40, 'bold'), command=partial(exchange, zero_ix))
 	b[zero_ix].grid(row=zero_ix//3, column=zero_ix%3, ipady=80)
 	b[index].grid_forget()
-	b[index] = Button(root, text=None, width=13, font=('Helvetica', 20, 'bold'), command=partial(exchange, index))
+	b[index] = Button(root, text=None, width=13, font=('Helvetica', 40, 'bold'), command=partial(exchange, index))
 	b[index].grid(row=index//3, column=index%3, ipady=80)
 	state[zero_ix], state[index] = state[index], state[zero_ix]
 	print('New state: ', state, '\n')
 
 
-b[0] = Button(root, text=f'{state[0]}' if state[0] != 0 else None, width=13, font=('Helvetica', 20, 'bold'), command=partial(exchange, 0))
+b[0] = Button(root, text=f'{state[0]}' if state[0] != 0 else None, width=13, font=('Helvetica', 40, 'bold'), command=partial(exchange, 0))
 b[0].grid(row=0, column=0, ipady=80)
-b[1] = Button(root, text=f'{state[1]}' if state[1] != 0 else None, width=13, font=('Helvetica', 20, 'bold'), command=partial(exchange, 1))
+b[1] = Button(root, text=f'{state[1]}' if state[1] != 0 else None, width=13, font=('Helvetica', 40, 'bold'), command=partial(exchange, 1))
 b[1].grid(row=0, column=1, ipady=80)
-b[2] = Button(root, text=f'{state[2]}' if state[2] != 0 else None, width=13, font=('Helvetica', 20, 'bold'), command=partial(exchange, 2))
+b[2] = Button(root, text=f'{state[2]}' if state[2] != 0 else None, width=13, font=('Helvetica', 40, 'bold'), command=partial(exchange, 2))
 b[2].grid(row=0, column=2, ipady=80)
-b[3] = Button(root, text=f'{state[3]}' if state[3] != 0 else None, width=13, font=('Helvetica', 20, 'bold'), command=partial(exchange, 3))
+b[3] = Button(root, text=f'{state[3]}' if state[3] != 0 else None, width=13, font=('Helvetica', 40, 'bold'), command=partial(exchange, 3))
 b[3].grid(row=1, column=0, ipady=80)
-b[4] = Button(root, text=f'{state[4]}' if state[4] != 0 else None, width=13, font=('Helvetica', 20, 'bold'), command=partial(exchange, 4))
+b[4] = Button(root, text=f'{state[4]}' if state[4] != 0 else None, width=13, font=('Helvetica', 40, 'bold'), command=partial(exchange, 4))
 b[4].grid(row=1, column=1, ipady=80)
-b[5] = Button(root, text=f'{state[5]}' if state[5] != 0 else None, width=13, font=('Helvetica', 20, 'bold'), command=partial(exchange, 5))
+b[5] = Button(root, text=f'{state[5]}' if state[5] != 0 else None, width=13, font=('Helvetica', 40, 'bold'), command=partial(exchange, 5))
 b[5].grid(row=1, column=2, ipady=80)
-b[6] = Button(root, text=f'{state[6]}' if state[6] != 0 else None, width=13, font=('Helvetica', 20, 'bold'), command=partial(exchange, 6))
+b[6] = Button(root, text=f'{state[6]}' if state[6] != 0 else None, width=13, font=('Helvetica', 40, 'bold'), command=partial(exchange, 6))
 b[6].grid(row=2, column=0, ipady=80)
-b[7] = Button(root, text=f'{state[7]}' if state[7] != 0 else None, width=13, font=('Helvetica', 20, 'bold'), command=partial(exchange, 7))
+b[7] = Button(root, text=f'{state[7]}' if state[7] != 0 else None, width=13, font=('Helvetica', 40, 'bold'), command=partial(exchange, 7))
 b[7].grid(row=2, column=1, ipady=80)
-b[8] = Button(root, text=f'{state[8]}' if state[8] != 0 else None, width=13, font=('Helvetica', 20, 'bold'), command=partial(exchange, 8))
+b[8] = Button(root, text=f'{state[8]}' if state[8] != 0 else None, width=13, font=('Helvetica', 40, 'bold'), command=partial(exchange, 8))
 b[8].grid(row=2, column=2, ipady=80)
 root.mainloop()
