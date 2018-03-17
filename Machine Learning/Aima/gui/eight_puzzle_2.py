2f1b3235b31cc22b1684d554b0f38307c307d175
AngryCracker <ads6may@gmail.com>
2018-03-17 23:57:44 +0530

    minor

--- Machine Learning/Aima/gui/eight_puzzle_2.py
+++ Machine Learning/Aima/gui/eight_puzzle_2.py
@@ -12,8 +12,8 @@ import utils
 
 root = Tk()
 
-state = (1, 2, 3, 4, 5, 6, 7, 8, 0)
-puzzle = EightPuzzle(state)
+state = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+puzzle = EightPuzzle(tuple(state))
 
 b = [None]*9
 
