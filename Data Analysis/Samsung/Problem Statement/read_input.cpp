50754377f2ebfc4ace266c5d6cedee11e7f297dc
Aman Deep Singh <ads6may@gmail.com>
2018-03-21 19:31:23 +0530

    minor

--- Data Analysis/Samsung/Problem Statement/read_input.cpp
+++ Data Analysis/Samsung/Problem Statement/read_input.cpp
@@ -2,13 +2,17 @@
 
 using namespace std;
 
+void operate() {
+
+}
+
 int main(int argc, char const *argv[])
 {
-	ifstream file(argv[1]);
+	ifstream file("Testing/input.cpp");
 	string linebuffer;
 
 	while (file && getline(file, linebuffer)) {
 		if (linebuffer.length() == 0) continue;
-		cout << "line";
+		cout << linebuffer;
 	}
 }
