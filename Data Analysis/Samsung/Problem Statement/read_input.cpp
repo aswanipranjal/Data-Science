73548a58a790c320cfda003d9c80512442a3fa67
Aman Deep Singh <ads6may@gmail.com>
2018-03-21 20:02:45 +0530

    minor

--- Data Analysis/Samsung/Problem Statement/read_input.cpp
+++ Data Analysis/Samsung/Problem Statement/read_input.cpp
@@ -2,18 +2,28 @@
 
 using namespace std;
 
-void operate() {
+void operate(string input) {
+	cout << "Ye dekho input: " << input << endl;
 
 }
 
 int main(int argc, char const *argv[])
 {
-	ifstream file("Testing/input.txt");
+	ifstream infile("Testing/input.txt");
+	ofstream outfile("Testing/output.txt")
 	string linebuffer;
 
-	cout << file << endl;
-	while (file && getline(file, linebuffer)) {
+	while (infile && getline(infile, linebuffer)) {
 		if (linebuffer.length() == 0) continue;
-		cout << linebuffer << endl;
+		string temp = linebuffer.substr(0, 5);
+		transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+		// cout << "Lower hua?" << temp << endl;
+		if (temp == "case #" || temp == "case#") {
+			// cout << "MoBhai Don Dekhe Porn";
+			continue;
+		}
+		else {
+			operate(linebuffer);
+		}
 	}
 }
